15d25e866063b4369db45be5c66c07607a5f1c7a
GitJournal <app@gitjournal.io>
2022-09-26 20:50:37 +0800

    [2022-09-26 12:50:37]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -27,7 +27,7 @@ begin
 print("load complete\n")
 search_string = Msf::Modules::Metadata::Search.parse_search_string("cve:2008")
 
-result = Msf::Modules::Metadata::Cache.instance.find(search_string)
+@result = Msf::Modules::Metadata::Cache.instance.find(search_string)
 
 $LOAD_PATH << "/var/lib/gems/3.0.0/gems/awesome_print-1.9.2/lib/"
 # $LOAD_PATH << "/var/lib/gems/3.0.0/gems/amazing_print-1.4.0/lib/"
