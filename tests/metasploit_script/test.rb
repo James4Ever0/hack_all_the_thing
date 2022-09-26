9c2aa3753590d96ec117f754091787b854ff528a
GitJournal <app@gitjournal.io>
2022-09-26 17:56:16 +0800

    [2022-09-26 09:56:14]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/test.rb
+++ tests/metasploit_script/test.rb
@@ -1,2 +1,2 @@
-print("meterpreter run ruby script")
+println("meterpreter run ruby script")
 # [-] Failed to load plugin from /root/Desktop/works/hack_all_the_thing/tests/metasploit_script/test.rb: No classes were loaded from /root/Desktop/works/hack_all_the_thing/tests/metasploit_script/test.rb in the Msf::Plugin namespace.
