15119325b10373effb2787859e7d2465bf60ddb5
GitJournal <app@gitjournal.io>
2022-09-26 19:17:01 +0800

    [2022-09-26 11:17:01]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -17,7 +17,7 @@ begin
   require Pathname.new(__FILE__).realpath.expand_path.parent.join('config', 'boot')
   require 'msfenv'
   require 'metasploit/framework/profiler'
-#   require 'metasploit/framework/command/console'
+  require 'metasploit/framework/command/console'
 
   Metasploit::Framework::Profiler.start
 #   Metasploit::Framework::Command::Console.start
