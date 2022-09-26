fee935153aac3b68fb6b1abf02f0c9be6a7b9310
GitJournal <app@gitjournal.io>
2022-09-26 21:05:39 +0800

    [2022-09-26 13:05:39]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -41,8 +41,6 @@ require 'amazing_print'
 # puts(result)
 # ap result
 
-data = [ false, 42, %w(forty two), { :now = > Time.now, :class => Time.now.class, :distance => 42e42 } ]
-ap data
 # ap data
 # how to pretty print?
 # gem install amazing_print
