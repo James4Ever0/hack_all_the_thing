fd234a0a5fe146533bad7e99fa55db665076f06b
GitJournal <app@gitjournal.io>
2022-09-26 20:49:01 +0800

    update

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -33,7 +33,8 @@ $LOAD_PATH << "/var/lib/gems/3.0.0/gems/amazing_print-1.4.0/lib/"
 # require '/var/lib/gems/3.0.0/gems/amazing_print-1.4.0'
 # use the gem inside the msfconsole.
 # maybe not.
-require 'amazing_print'
+# require 'amazing_print'
+require 'ap'
 # how about let's use absolute import path?
 # print(result)
 # puts(result)
