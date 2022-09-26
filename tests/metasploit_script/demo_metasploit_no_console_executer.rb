2cd6980ab4953a2e750b62b34cbe277beab21560
GitJournal <app@gitjournal.io>
2022-09-26 20:50:45 +0800

    [2022-09-26 12:50:45]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -39,7 +39,7 @@ require 'ap'
 # how about let's use absolute import path?
 # print(result)
 # puts(result)
-ap result
+ap @result
 # data = [ false, 42, %w(forty two), { :now => Time.now, :class => Time.now.class, :distance => 42e42 } ]
 # ap data
 # how to pretty print?
