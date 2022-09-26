4feb8dfd419332e87205e69ebb59626108f07a0c
GitJournal <app@gitjournal.io>
2022-09-26 19:18:01 +0800

    [2022-09-26 11:18:01]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -14,6 +14,7 @@ begin
   end
 
   # @see https://github.com/rails/rails/blob/v3.2.17/railties/lib/rails/generators/rails/app/templates/script/rails#L3-L5
+  ## this is probably not the file path.
   require Pathname.new(__FILE__).realpath.expand_path.parent.join('config', 'boot')
   require 'msfenv'
   require 'metasploit/framework/profiler'
