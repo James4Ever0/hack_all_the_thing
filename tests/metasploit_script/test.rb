f5fb34463753e4dd0f032d3713b638743e59e6a8
GitJournal <app@gitjournal.io>
2022-09-26 17:57:01 +0800

    update

--- tests/metasploit_script/test.rb
+++ tests/metasploit_script/test.rb
@@ -1,3 +1,3 @@
 print("meterpreter run ruby script\n")
 # [-] Failed to load plugin from /root/Desktop/works/hack_all_the_thing/tests/metasploit_script/test.rb: No classes were loaded from /root/Desktop/works/hack_all_the_thing/tests/metasploit_script/test.rb in the Msf::Plugin namespace.
-print("another line")
+print("another line\n")
