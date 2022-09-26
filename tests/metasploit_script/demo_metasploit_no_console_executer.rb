51865db95aba2d9bdf2534db66138f131f9cb86d
GitJournal <app@gitjournal.io>
2022-09-26 19:31:01 +0800

    update

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -29,7 +29,7 @@ search_string = Msf::Modules::Metadata::Search.parse_search_string("cve:2008")
 
 result = Msf::Modules::Metadata::Cache.instance.find(search_string)
 
-require '/var/lib/gems/3.0.0/gems/amazing_print'
+require '/var/lib/gems/3.0.0/gems/amazing_print-1.4.0'
 # how about let's use absolute import path?
 # print(result)
 # puts(result)
