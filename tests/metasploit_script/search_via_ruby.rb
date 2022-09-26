d3dd60ae12e50f089c04c8b1e811a64703a33c8b
GitJournal <app@gitjournal.io>
2022-09-26 19:22:01 +0800

    update

--- tests/metasploit_script/search_via_ruby.rb
+++ tests/metasploit_script/search_via_ruby.rb
@@ -8,9 +8,11 @@ puts('hello world')
 
 # you are going to dig into the msf source code.
 # can you run bare ruby script instead of this heavy shit?
+
 search_string = Msf::Modules::Metadata::Search.parse_search_string("cve:2008")
 
 result = Msf::Modules::Metadata::Cache.instance.find(search_string)
+
 # there used to be an @ before.
 puts("search end")
 print(result)
