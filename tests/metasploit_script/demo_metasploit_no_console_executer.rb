6578655c7a093041e1a692cc855966eeaa258844
GitJournal <app@gitjournal.io>
2022-09-26 19:30:00 +0800

    [2022-09-26 11:30:00]
    FILE:
    tests/metasploit_script/demo_metasploit_no_console_executer.rb
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -30,6 +30,7 @@ search_string = Msf::Modules::Metadata::Search.parse_search_string("cve:2008")
 result = Msf::Modules::Metadata::Cache.instance.find(search_string)
 
 require 'amazing_print'
+# how about let's use absolute import path?
 # print(result)
 # puts(result)
 # how to pretty print?
