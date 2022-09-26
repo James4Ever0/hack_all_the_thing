e319d7870ed3ee4f1f2de202a6364bc87f9e07cf
GitJournal <app@gitjournal.io>
2022-09-26 20:54:01 +0800

    update

--- tests/metasploit_script/demo_metasploit_no_console_executer.rb
+++ tests/metasploit_script/demo_metasploit_no_console_executer.rb
@@ -79,6 +79,7 @@ end
 
 # data = [ false, 42, %w(forty two), { :now => Time.now, :class => Time.now.class, :distance => 42e42 } ]
 # ap data
+pm data
 # how to pretty print?
 # gem install amazing_print
 $LOAD_PATH << "." # must add this line
