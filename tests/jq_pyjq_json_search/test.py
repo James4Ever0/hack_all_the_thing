82bada662d6aec6631d9e6209853d70255156040
modifier <termux@termux.com>
2022-09-27 20:34:47 +0800

    [2022-09-27 12:34:46]
    FILE:
    tests/jq_pyjq_json_search/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jq_pyjq_json_search/test.py
+++ tests/jq_pyjq_json_search/test.py
@@ -19,7 +19,7 @@ mdict3={"Abc":"dEF","a":[1,2,{'de':{'t':'uwy'}}]}
 #expression='to_entries'
 #[[{'key': 'Abc', 'value': 'dEF'}, {'key': 'a', 'value': [1, 2, {'de': {'t': 'uwy'}}]}]]
 # not so good?
-expression='.[]'
+#expression='.[]' # unroll for 1 layer
 values=pyjq.all(expression,mdict3)
 #values=pyjq.all(expression,mdict2)
 #values=pyjq.all(expression,mdict)
