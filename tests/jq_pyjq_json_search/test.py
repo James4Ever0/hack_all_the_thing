a60da90575f5dc321d361240c6e7c41034f2f079
modifier <termux@termux.com>
2022-09-27 19:55:03 +0800

    [2022-09-27 11:55:01]
    FILE:
    tests/jq_pyjq_json_search/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jq_pyjq_json_search/test.py
+++ tests/jq_pyjq_json_search/test.py
@@ -5,8 +5,10 @@ mdict={k:mdict for k in "ghi"}
 
 import pyjq
 
-expression='.. try contains("def") catch false'
+#expression='.. try contains("def") catch false'
 #expression='paths(type=="string")'
+#
+expression='paths(try contains("def") catch false)'
 values=pyjq.all(expression,mdict)
 print("mdict:",mdict)
 print("values:", values)
