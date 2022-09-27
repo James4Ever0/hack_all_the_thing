a27e8d79b5ca6933729b2180c617e76666df419e
modifier <termux@termux.com>
2022-09-27 19:13:01 +0800

    [2022-09-27 11:13:00]
    FILE:
    tests/jq_pyjq_json_search/test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/jq_pyjq_json_search/test.py
+++ tests/jq_pyjq_json_search/test.py
@@ -5,7 +5,7 @@ mdict={k:mdict for k in "ghi"}
 
 import pyjq
 
-expression='paths(string)'
+expression='paths(type=="string")'
 values=pyjq.all(expression,mdict)
 print("mdict:",mdict)
 print("values:", values)
