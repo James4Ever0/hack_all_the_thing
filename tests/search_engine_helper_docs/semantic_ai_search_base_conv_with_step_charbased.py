e28408e9173ff8acddb0c163a083823a701ee521
GitJournal <app@gitjournal.io>
2022-09-29 01:38:56 +0800

    [2022-09-28 17:38:56]
    FILE:
    tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
+++ tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
@@ -55,6 +55,7 @@ group_per_conv_group = 3
 step_group_for_conv = 2  # instead of 1. just to make sure these conv groups overlap.
 
 assert step_group_for_conv <= group_per_conv_group # at least there is no gap, though when equal there will be no overlapping.
+assert group_per_conv_group == step_group_
 # rule to add space: if there's "-" ending, remove the "-" then directly concat with another line.
 # if not, then make sure there's one space between two lines.
 # create char index to line index mapping.
