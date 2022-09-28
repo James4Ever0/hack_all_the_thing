eb9989ab49035c0b00f33f6a646d3e8646e136e3
GitJournal <app@gitjournal.io>
2022-09-29 05:00:24 +0800

    [2022-09-28 21:00:24]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -35,7 +35,6 @@ if __name__ == "__main__":
         # Dot product on normalized vectors is equal to cosine similarity
 
         # Add index and sort desc based on score
-    uids = [sorted(enumerate(score), key=lambda x: x[1], reverse=True) for score in scores]
     while True:
         query = input("> ")
         if query == "q":
@@ -46,6 +45,7 @@ if __name__ == "__main__":
         scores = np.dot(queries, data.T).tolist()
 
         # uid = embeddings.similarity(query, data)[0][0]
+        uids = [sorted(enumerate(score), key=lambda x: x[1], reverse=True) for score in scores]
         uid = uids[0][0][0]
         answer = data_source[uid]
         print(answer)
