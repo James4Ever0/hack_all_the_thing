8ced9777158139e6a425de0dd894272d736dc836
GitJournal <app@gitjournal.io>
2022-09-28 18:50:34 +0800

    [2022-09-28 10:50:33]
    FILE:
    tests/search_engine_helper_docs/myBeeper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/myBeeper.py
+++ tests/search_engine_helper_docs/myBeeper.py
@@ -4,7 +4,9 @@ from textual.app import App
 class Beeper(App):
     def on_key(self):
         # self.console.bell()
-        self.console.print('some text')
+        self.console.print('some text here')
+        it will print from bottom up, accumulatively.
+        
 
 
 Beeper.run()
