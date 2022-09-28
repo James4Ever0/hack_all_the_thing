39df52142c200d0c0eabe447fd5818e6603c9037
GitJournal <app@gitjournal.io>
2022-09-29 01:28:28 +0800

    [2022-09-28 17:28:28]
    FILE:
    tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
+++ tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
@@ -101,6 +101,7 @@ while True:
         "conv_group_merged": newContent[startIndex : startIndex + endIndexOffset],
         "line_range": lineIndicesTuple,
     }
+    listOfCleanedMergedConvGroupWithLineIndexMapping.append(mElem)
 
     # add to startIndex.
     startIndex += step_group_for_conv * char_per_group
