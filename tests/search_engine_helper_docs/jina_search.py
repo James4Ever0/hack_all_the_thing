df4caacc125f8d9eeae62519a7ba3e4e949bb19f
GitJournal <app@gitjournal.io>
2022-09-29 04:07:54 +0800

    [2022-09-28 20:07:54]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -39,7 +39,7 @@ q = (
 )
 
 # print(q.matches[:5, ("text", "scores__jaccard__value")])
-docArray_5 = q.matches[:5]
+docArray_5 = q.matches[:5, ("text")]
 # do we have other things?
 # print(q.embedding) # no embedding! wtf?
 # {'multi_modal_schema': {'content': {'attribute_type': <AttributeType.DOCUMENT: 'document'>, 'type': 'Text', 'position': 0}}}
