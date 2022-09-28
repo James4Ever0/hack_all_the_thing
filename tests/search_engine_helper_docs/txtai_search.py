d9786643a48b21b20caf4b3f96e0cfc36c061e99
GitJournal <app@gitjournal.io>
2022-09-29 04:46:10 +0800

    [2022-09-28 20:46:10]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -30,15 +30,6 @@ class Application:
         st.title("Similarity Search")
         st.markdown("This application runs a basic similarity search that identifies the best matching row for a query.")
 
-        data = [
-            "US tops 5 million confirmed virus cases",
-            "Canada's last fully intact ice shelf has suddenly collapsed, forming a Manhattan-sized iceberg",
-            "Beijing mobilises invasion craft along coast as Taiwan tensions escalate",
-            "The National Park Service warns against sacrificing slower friends in a bear attack",
-            "Maine man wins $1M from $25 lottery ticket",
-            "Make huge profits without work, earn up to $100,000 a day",
-        ]
-
 
         if query:
             # Get index of best section that best matches query
