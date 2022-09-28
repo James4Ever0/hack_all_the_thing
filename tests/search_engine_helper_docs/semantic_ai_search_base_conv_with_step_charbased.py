463fa7b4287d15be58b2a136d04ef3fb8a27f8f1
GitJournal <app@gitjournal.io>
2022-09-29 01:43:50 +0800

    [2022-09-28 17:43:50]
    FILE:
    tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
+++ tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
@@ -90,7 +90,7 @@ print("MAX KEY:", max(list(newContentCharIndexToLineIndexDict.keys())))
 # MAX KEY: 85783
 # which is smaller than:
 # KeyError: 85830
-# so it is obvious that we need the smaller 'end
+# so it is obvious that we need the smaller 'endIndex'
 breakpoint()
 
 newContentLength = len(newContent)
