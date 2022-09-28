39c236666901672c936872539cb417b223c490ca
GitJournal <app@gitjournal.io>
2022-09-29 04:07:35 +0800

    [2022-09-28 20:07:35]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -4,7 +4,7 @@ from semantic_ai_search_base_conv_with_step_charbased import (
     linewise,
 )
 
-query = "math addition operation" # strange enbedd
+query = "math addition operation" # strange enbedding!
 
 from docarray import Document, DocumentArray
 
