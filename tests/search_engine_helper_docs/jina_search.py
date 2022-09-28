1144d9e0ea27c957331e5ca7487850f0bae44e2c
GitJournal <app@gitjournal.io>
2022-09-29 04:11:42 +0800

    [2022-09-28 20:11:42]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -6,7 +6,7 @@ from semantic_ai_search_base_conv_with_step_charbased import (
 
 # query = "math addition operation" # strange enbedding!
 # query="following examples"
-query=
+query="math" # not common maybe
 from docarray import Document, DocumentArray
 
 # from docarray import dataclass
