ed06690fb7af210501e4ea32a6cc6f5fa1340105
GitJournal <app@gitjournal.io>
2022-09-28 18:51:16 +0800

    [2022-09-28 10:51:16]
    FILE:
    tests/search_engine_helper_docs/myBeeper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/myBeeper.py
+++ tests/search_engine_helper_docs/myBeeper.py
@@ -4,6 +4,7 @@ from textual.app import App
 class Beeper(App):
     def on_key(self, event):
         # self.console.bell()
+        key = event.key
         self.console.print('some text here')
         # it will print from bottom up, accumulatively.
         # but what is the fucking key?
