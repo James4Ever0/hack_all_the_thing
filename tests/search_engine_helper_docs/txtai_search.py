67d5012149aa751a30d10e78bad7d16ffa0bb58f
GitJournal <app@gitjournal.io>
2022-09-29 00:31:24 +0800

    [2022-09-28 16:31:24]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -52,7 +52,7 @@ def standardLineCleaner(line):
 char_per_line = 
 
 # rule to add space: if there's "-" ending, remove the "-" then directly concat with another line.
-# if not, then make sure there's one space between w
+# if not, then make sure there's one space between two lines.
 
 for lineNumber, line in enumerate(linewise):
     line_cleaned = standardLineCleaner(line)
