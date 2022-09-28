5b333a7990db66fbed4105423bfb2c14c254c76f
GitJournal <app@gitjournal.io>
2022-09-28 21:16:02 +0800

    [2022-09-28 13:16:02]
    FILE:
    tests/search_engine_helper_docs/whoosh_highlight_alone.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_highlight_alone.py
+++ tests/search_engine_helper_docs/whoosh_highlight_alone.py
@@ -6,25 +6,40 @@ query = "math addition"
 # we need exact location of the thing! scroll to it and display it!
 from whoosh.highlight import highlight
 
-text =data
-terms = set(query.split(" ")) # what is this terms?
+text = data
+terms = set(query.split(" "))  # what is this terms?
 # A sequence or set containing the query words to match, e.g. (“render”, “shader”).
-formatter = 
 
-from whoosh.highlight import BasicFragmentScorer, FIRST, ContextFragmenter, NullFormatter
+from whoosh.highlight import (
+    BasicFragmentScorer,
+    FIRST,
+    ContextFragmenter,
+    NullFormatter,
+)
+
 
 class ListFormatter(NullFormatter):
     def format(self, fragments, replace=False):
         # return list this time.
-        formatted = [self.format_fragment(f, replace=replace)
-                        for f in fragments]
+        formatted = [self.format_fragment(f, replace=replace) for f in fragments]
         return formatted
+
+
 from whoosh.analysis import StandardAnalyzer
+
 charlimit = 1000000
 analyzer = StandardAnalyzer
-fragmenter = ContextFragmenter(charlimit = charlimit)
+fragmenter = ContextFragmenter(charlimit=charlimit)
 formatter = ListFormatter()
 
-excerpts = highlight(text, terms, analyzer, fragmenter, formatter, top=3,
-                     scorer=BasicFragmentScorer, minscore=1, order=FIRST
-                     )
+excerpts = highlight(
+    text,
+    terms,
+    analyzer,
+    fragmenter,
+    formatter,
+    top=3,
+    scorer=BasicFragmentScorer,
+    minscore=1,
+    order=FIRST,
+)
