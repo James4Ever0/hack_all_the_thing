b988ffb3b2294b47f623ea5b0aa161bd88220cee
GitJournal <app@gitjournal.io>
2022-09-28 11:46:35 +0800

    [2022-09-28 03:46:35]
    FILE:
    tests/search_engine_helper_docs/textual_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_test.py
+++ tests/search_engine_helper_docs/textual_test.py
@@ -1,5 +1,6 @@
 from rich.text import Text
-text = Text("hello world")
+text = Text("hello world python this is python")
 text.on(click="view.toggle('world')")
 
 text.highlight_words('python','bold red')
+print(text)
