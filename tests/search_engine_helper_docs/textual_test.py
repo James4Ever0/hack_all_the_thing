fa73c6609fd235813a0a97d002e61e5210587711
GitJournal <app@gitjournal.io>
2022-09-28 18:37:15 +0800

    [2022-09-28 10:37:15]
    FILE:
    tests/search_engine_helper_docs/textual_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_test.py
+++ tests/search_engine_helper_docs/textual_test.py
@@ -2,7 +2,9 @@ from rich.text import Text
 text = Text("hello world python this is python", style='red')
 text.on(click="view.toggle('world')")
 
-highlighted = text.highlight_words('python',style='yellow')
-print(text)/
+# highlighted = text.highlight_words('python',style='yellow')
+
+# print(text)
+# print(highlighted)
 
 # after successfully bricked my damn lenovo device, i realized that there is nothing good using such a restricted shit.
