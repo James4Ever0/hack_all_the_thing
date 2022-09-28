28080717d6b1e0b49c09b6c35eeaa00e2d8a1296
GitJournal <app@gitjournal.io>
2022-09-29 04:00:07 +0800

    [2022-09-28 20:00:07]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -47,6 +47,7 @@ mDocument = docArray_5[0]
 print(mDocument.source.text)
 print(mDocument.text)
 print(mDocument.lineRange.tags)
+[1933, 1936]
 
 # <Document ('id', 'adjacency', 'text', 'tags', 'embedding', 'scores') at 5b7fb3671d405bdb1840b2c2a2fd7c2f>
 # we do not have other fields.
