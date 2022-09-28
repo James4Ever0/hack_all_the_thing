b8da342aa7397d1559f47056d3b2b8f53494cf27
GitJournal <app@gitjournal.io>
2022-09-28 20:27:12 +0800

    [2022-09-28 12:27:11]
    FILE:
    tests/search_engine_helper_docs/whoosh_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_highlight.py
+++ tests/search_engine_helper_docs/whoosh_highlight.py
@@ -48,7 +48,7 @@ def index_search(dirname, search_fields, search_query):
         for hit in results:
             # highlights = hit.highlights('content', top=5) # str. not list.
             # highlights = hit.highlights('content', text=data)
-            hit.
+            hit.fragments('content')
             # it is multi-sourced. separaed by three dots.
             # how it fucking works?
             # highlights = hit.highlights('content')
