5c6a00af47636905e6f9dd6866295ea00acb1964
GitJournal <app@gitjournal.io>
2022-09-29 03:26:55 +0800

    [2022-09-28 19:26:55]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -1,17 +1,23 @@
-from semantic_ai_search_base_conv_with_step_charbased import listOfCleanedMergedConvGroupWithLineIndexMapping, linewise
+from semantic_ai_search_base_conv_with_step_charbased import (
+    listOfCleanedMergedConvGroupWithLineIndexMapping,
+    linewise,
+)
 
 query = "math addition"
 
 from docarray import Document, DocumentArray
 
 # d = Document(uri='https://www.gutenberg.org/files/1342/1342-0.txt').load_uri_to_text()
-da = DocumentArray(Document(text=elem['conv_group_merged'], line_span=elem['line_range']) for elem in listOfCleanedMergedConvGroupWithLineIndexMapping )
-da.apply(Document.embed_feature_hashing, backend='process')
+da = DocumentArray(
+    Document(text=elem["conv_group_merged"], line_span=elem["line_range"])
+    for elem in listOfCleanedMergedConvGroupWithLineIndexMapping
+)
+da.apply(Document.embed_feature_hashing, backend="process")
 
 q = (
     Document(text=query)
     .embed_feature_hashing()
-    .match(da, metric='jaccard', use_scipy=True)
+    .match(da, metric="jaccard", use_scipy=True)
 )
 
-print(q.matches[:5, ('text', 'scores__jaccard__value')])
+print(q.matches[:5, ("text", "scores__jaccard__value")])
