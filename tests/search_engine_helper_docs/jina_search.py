4472d5143140d1c6cb15d8d819f4e63c72815fa0
GitJournal <app@gitjournal.io>
2022-09-29 02:21:37 +0800

    [2022-09-28 18:21:37]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -5,7 +5,7 @@ query = "math addition"
 from docarray import Document, DocumentArray
 
 # d = Document(uri='https://www.gutenberg.org/files/1342/1342-0.txt').load_uri_to_text()
-da = DocumentArray(Document(text=) for s in d.text.split('\n') if s.strip())
+da = DocumentArray(Document(text=elem['']) for elem in )
 da.apply(Document.embed_feature_hashing, backend='process')
 
 q = (
