702d0c9c1fffd1af29b9a2b84d434ce733061c11
GitJournal <app@gitjournal.io>
2022-09-29 03:54:51 +0800

    [2022-09-28 19:54:50]
    FILE:
    tests/search_engine_helper_docs/jina_docarray_multimodal.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/search_engine_helper_docs/jina_docarray_multimodal.py
@@ -0,0 +1,21 @@
+from docarray import dataclass, Document
+from docarray.typing import Image, Text, JSON
+
+
+@dataclass
+class WPArticle:
+    banner: Image
+    headline: Text
+    meta: JSON
+
+
+a = WPArticle(
+    banner='https://.../cat-dog-flight.png',
+    headline='Everything to know about flying with pets, ...',
+    meta={
+        'author': 'Nathan Diller',
+        'Column': 'By the Way - A Post Travel Destination',
+    },
+)
+
+d = Document(a)
