3560e395d18cc844429217a5cab90dbf4e0b400d
GitJournal <app@gitjournal.io>
2022-09-28 18:53:21 +0800

    [2022-09-28 10:53:21]
    FILE:
    tests/search_engine_helper_docs/myBeeper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/myBeeper.py
+++ tests/search_engine_helper_docs/myBeeper.py
@@ -8,6 +8,7 @@ class Beeper(App):
         # self.console.print('some text here')
         print(dir(key))
         self.console.print(key)
+        # seems to be a string like thing.
         # will pretty print the dir thing. fuck.
         # it will print from bottom up, accumulatively.
         # but what is the fucking key?
