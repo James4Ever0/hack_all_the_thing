0f420ac5a2ae9834984c12117f9499cd5c8f3c43
GitJournal <app@gitjournal.io>
2022-09-29 01:50:25 +0800

    [2022-09-28 17:50:23]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
+++ tests/search_engine_helper_docs/semantic_ai_search_base_conv_with_step_charbased.py
@@ -134,5 +134,6 @@ if __name__ == "__main__":
     sprint(the_final_one["conv_group_merged"])
     start, end = the_final_one["line_range"]
     myLines = linewise[start:end+1] # this is inclusive!
+    # s
     for line in myLines:
         print(line)
