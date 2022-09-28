974d20999cd7752c647c906e02e8e4a21153e24d
GitJournal <app@gitjournal.io>
2022-09-29 03:46:21 +0800

    [2022-09-28 19:46:21]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -32,6 +32,7 @@ docArray_5 = q.matches[:5]
 
 print(docArray_5[0])
 # <Document ('id', 'adjacency', 'text', 'tags', 'embedding', 'scores') at 5b7fb3671d405bdb1840b2c2a2fd7c2f>
+# we do not have other fields.
 # it is not good! we do not have anything related to 'math' or 'addition' or 'operation'!
 
 # still nothing? wtf?
