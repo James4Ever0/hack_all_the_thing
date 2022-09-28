5f894d52a50375a210cd173c81ef1dcf45a882cb
GitJournal <app@gitjournal.io>
2022-09-29 00:10:33 +0800

    [2022-09-28 16:10:33]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -24,7 +24,7 @@ def removeDuplicates(line, chars=[" ","\t"], maxConsecutiveLength=1):
         minUnallowedConsecutiveLength = maxConsecutiveLength +1
         while True:
             if char*minUnallowedConsecutiveLength in line:
-                line = line.replace()
+                line = line.replace(char, char*minUnallowedConsecutiveLength, char*maxConsecutiveLength))
 
 def lstripChars(line, chars=[" ","\t"]):
 
