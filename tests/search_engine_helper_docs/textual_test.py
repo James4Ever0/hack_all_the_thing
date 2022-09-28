56d0f5e51c0929279dae589b6db3d4632b148dea
GitJournal <app@gitjournal.io>
2022-09-28 18:39:29 +0800

    [2022-09-28 10:39:29]
    FILE:
    tests/search_engine_helper_docs/textual_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_test.py
+++ tests/search_engine_helper_docs/textual_test.py
@@ -4,7 +4,7 @@ text.on(click="view.toggle('world')")
 from rich.console import Console
 console = Console()
 highlighted = text.highlight_words(['python'],style='yellow') # but we should not highlight individual letters right?
-# so we pass a list of 
+# so we pass a list of words to be highlighted.
 # just a damn number?
 console.print(text) # red.
 # print(highlighted)
