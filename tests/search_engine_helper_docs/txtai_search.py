69c4a86b5caa1e004280349698359e3408b72df7
GitJournal <app@gitjournal.io>
2022-09-29 04:47:44 +0800

    [2022-09-28 20:47:44]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -8,16 +8,6 @@ from txtai.embeddings import Embeddings
 # 1455MiB. this is high.
 # this is a search application! damn it.
 
-        embeddings = Embeddings({"path": "sentence-transformers/nli-mpnet-base-v2"})
-        # loading this thing?
-        # warning! this is a huge model. could crash my freaking device.
-
-        # Get index of best section that best matches query
-        uid = self.embeddings.similarity(query, data)[0][0]
-        answer = data[uid]
-
-
-
 
 if __name__ == "__main__":
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
