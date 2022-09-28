b81fbd434f74be258068b4404661190444376d45
GitJournal <app@gitjournal.io>
2022-09-28 18:55:25 +0800

    [2022-09-28 10:55:25]
    FILE:
    tests/search_engine_helper_docs/myBeeper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/myBeeper.py
+++ tests/search_engine_helper_docs/myBeeper.py
@@ -8,7 +8,7 @@ class Beeper(App):
         # self.console.print('some text here')
         print(dir(key))
         self.console.print(key)
-        capitalize, casefold, center, count, encode, endswith, expandtabs, find, format, format_map, index, isalnum, isalpha, isascii, isdecimal, isdigit, is
+        capitalize, casefold, center, count, encode, endswith, expandtabs, find, format, format_map, index, isalnum, isalpha, isascii, isdecimal, isdigit, isidentidier, islower, isnumeric, isprintable, isspace, istitle, isupper, join, ljust, lower, lstrip, maketrans, partition, removeprefix, removesuffix, 
         # seems to be a string like thing.
         # will pretty print the dir thing. fuck.
         # it will print from bottom up, accumulatively.
