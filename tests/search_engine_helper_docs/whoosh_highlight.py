4f0745d2a8c5b1e2550db338f2bd5a56a688f8bc
GitJournal <app@gitjournal.io>
2022-09-28 20:06:25 +0800

    [2022-09-28 12:06:25]
    FILE:
    tests/search_engine_helper_docs/whoosh_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_highlight.py
+++ tests/search_engine_helper_docs/whoosh_highlight.py
@@ -4,3 +4,16 @@ from load_demo_data import data
 
 # remember this shit is long!
 
+from whoosh.fields import Schema, TEXT, ID
+from whoosh import index
+import os, os.path
+from whoosh import index
+from whoosh import qparser
+from whoosh.qparser import QueryParser
+
+schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored = True))
+
+# create empty index directory
+
+if not os.path.exists("index_dir"):
+    os.mkdir("index_dir")
