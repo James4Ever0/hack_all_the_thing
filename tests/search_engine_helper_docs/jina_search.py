345508d762e3496a94453d33ba938d5f3d8fb955
GitJournal <app@gitjournal.io>
2022-09-29 03:38:17 +0800

    [2022-09-28 19:38:17]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -31,7 +31,7 @@ da = DocumentArray(
 da.apply(Document.embed_feature_hashing, backend="process")
 
 q = (
-    Document(WPExcerpt(content=query,source='jq_man.log',lineRange=[10,20]))
+    Document(WPExcerpt(content=query,source=None,lineRange=None))
     .embed_feature_hashing()
     .match(da, metric="jaccard", use_scipy=True)
 )
