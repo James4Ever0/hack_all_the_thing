978facabaa0e28f8f9ac7b0718e249a8ae2911b0
GitJournal <app@gitjournal.io>
2022-09-29 00:26:46 +0800

    [2022-09-28 16:26:46]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -49,9 +49,10 @@ def standardLineCleaner(line):
     line = lstripChars(line)
     return line
 
+co
 
 for lineNumber, line in enumerate(linewise):
     line_cleaned = standardLineCleaner(line)
-    print("{}:".format(lineNumber), line_cleaned)
+    # print("{}:".format(lineNumber), line_cleaned)
 
 # we decide to join lines with space no matter what. afterwards we use standard cleaner to remove duplicate spaces.
