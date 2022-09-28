c4b1a2c9d9841a9fbdba0a50aa11f3ba03400cf9
GitJournal <app@gitjournal.io>
2022-09-28 20:58:23 +0800

    [2022-09-28 12:58:23]
    FILE:
    tests/search_engine_helper_docs/whoosh_highlight_alone.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_highlight_alone.py
+++ tests/search_engine_helper_docs/whoosh_highlight_alone.py
@@ -8,6 +8,7 @@ from whoosh.highlight import highlight
 
 text =data
 terms = query # what is this terms?
+# A sequence or set containing the query words to match, e.g. (“render”, “shader”).
 
 excerpts = highlight(text, terms, analyzer, fragmenter, formatter, top=3,
                      scorer=BasicFragmentScorer, minscore=1, order=FIRST)
