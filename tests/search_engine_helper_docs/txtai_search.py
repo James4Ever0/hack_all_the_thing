301fac24de29e804fd2914cee7dd1e844c664752
GitJournal <app@gitjournal.io>
2022-09-28 23:41:50 +0800

    [2022-09-28 15:41:50]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -16,7 +16,7 @@ linewise = data.split("\n")
 
 def removeDuplicates(line, char=" "):
 
-def lstripChars(l)
+def lstripChars(line, chars=)
 
 for lineNumber, line in enumerate(linewise):
     print("{}:".format(lineNumber),line)
