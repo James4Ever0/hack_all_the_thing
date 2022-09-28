2cf72acd91ab500e97858bc9fd229c06ca2f0d9d
GitJournal <app@gitjournal.io>
2022-09-28 18:27:51 +0800

    [2022-09-28 10:27:51]
    FILE:
    tests/search_engine_helper_docs/textual_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_test.py
+++ tests/search_engine_helper_docs/textual_test.py
@@ -4,4 +4,4 @@ text.on(click="view.toggle('world')")
 
 text.highlight_words('python','bold red')
 print(text)
-# after successifully bricked my damn lenovo device, i realized that there is nothing good using such an rest
+# after successifully bricked my damn lenovo device, i realized that there is nothing good using such an restricted shit.
