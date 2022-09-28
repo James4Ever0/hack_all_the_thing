b3d89a2e8977a7a0e098cd2754b2416409f06245
GitJournal <app@gitjournal.io>
2022-09-29 04:41:33 +0800

    [2022-09-28 20:41:33]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -7,7 +7,7 @@ from txtai.embeddings import Embeddings
 # seems it can pull out the right thing.
 # but the ram consumption?
 # 1455MiB. this is high.
-# this is a search application! damn it. 
+# this is a search application! damn it.
 
 class Application:
     """
