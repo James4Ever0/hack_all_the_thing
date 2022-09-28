6f1f8acc6604fbb56bf7132d64becc086596bcdb
GitJournal <app@gitjournal.io>
2022-09-29 02:19:49 +0800

    [2022-09-28 18:19:49]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -2,3 +2,16 @@ from semantic_ai_search_base_conv_with_step_charbased import listOfCleanedMerged
 
 query = "math addition"`
 
+from docarray import Document, DocumentArray
+
+d = Document(uri='https://www.gutenberg.org/files/1342/1342-0.txt').load_uri_to_text()
+da = DocumentArray(Document(text=s.strip()) for s in d.text.split('\n') if s.strip())
+da.apply(Document.embed_feature_hashing, backend='process')
+
+q = (
+    Document(text='she smiled too much')
+    .embed_feature_hashing()
+    .match(da, metric='jaccard', use_scipy=True)
+)
+
+print(q.matches[:5, ('text', 'scores__jaccard__value')])
