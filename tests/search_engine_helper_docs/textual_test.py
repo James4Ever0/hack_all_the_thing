8036b62598f5206f680328e2280cad27fd86056a
GitJournal <app@gitjournal.io>
2022-09-28 18:42:12 +0800

    [2022-09-28 10:42:12]
    FILE:
    tests/search_engine_helper_docs/textual_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_test.py
+++ tests/search_engine_helper_docs/textual_test.py
@@ -1,12 +1,18 @@
 from rich.text import Text
-text = Text("hello world python this is python", style='green') # there is no style applied.
+
+text = Text(
+    "hello world python this is python", style="green"
+)  # there is no style applied.
 text.on(click="view.toggle('world')")
 from rich.console import Console
+
 console = Console()
-highlighted = text.highlight_words(['python'],style='yellow') # but we should not highlight individual letters right?
+highlighted = text.highlight_words(
+    ["python"], style="yellow"
+)  # but we should not highlight individual letters right?
 # so we pass a list of words to be highlighted.
 # just a damn number?
-console.print(text) # red.
+console.print(text)  # red.
 # now we have color. what else?
 # we need interface!
 # print(highlighted)
