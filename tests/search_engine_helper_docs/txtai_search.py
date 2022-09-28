db30247cec837919a66d60fd5a3b1ed94beca0e9
GitJournal <app@gitjournal.io>
2022-09-29 04:50:03 +0800

    [2022-09-28 20:50:03]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # Get index of best section that best matches query
     while True:
-    query = input("> ")
-    uid = embeddings.similarity(query, data)[0][0]
-    answer = data[uid]
-    print(answer)
+        query = input("> ")
+        uid = embeddings.similarity(query, data)[0][0]
+        answer = data[uid]
+        print(answer)
