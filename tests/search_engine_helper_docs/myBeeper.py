b6e82dca355694ca3adcaa92960a0edd9752edab
GitJournal <app@gitjournal.io>
2022-09-28 18:56:52 +0800

    [2022-09-28 10:56:52]
    FILE:
    tests/search_engine_helper_docs/myBeeper.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/myBeeper.py
+++ tests/search_engine_helper_docs/myBeeper.py
@@ -7,9 +7,9 @@ class Beeper(App):
         key = event.key
         # self.console.print('some text here')
         # print(dir(key))
-        print(type(key))
+        # print(type(key))
+        # it is str. fuck.
         self.console.print(key)
-        # looks like a bloated 'str' class.
         # capitalize, casefold, center, count, encode, endswith, expandtabs, find, format, format_map, index, isalnum, isalpha, isascii, isdecimal, isdigit, isidentidier, islower, isnumeric, isprintable, isspace, istitle, isupper, join, ljust, lower, lstrip, maketrans, partition, removeprefix, removesuffix, replace, rfind, rindex, rjust, rpartition, rsplit, rstrip, split, splitlines, startswith, strip, swapcase, title, translate, upper, zfill
         # seems to be a string like thing.
         # will pretty print the dir thing. fuck.
