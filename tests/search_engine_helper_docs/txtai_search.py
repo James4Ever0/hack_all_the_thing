c9121d71d71f70c7e1dbe83b5703b9dc5041434f
GitJournal <app@gitjournal.io>
2022-09-29 00:18:20 +0800

    [2022-09-28 16:18:20]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -13,36 +13,43 @@ from load_demo_data import data
 # since we do not have the 'keyword' based highlighter, we can only do line-wise highlighting.
 
 # where's the crlf -> lf ?
-data = data.replace("\r\n","\n")
+data = data.replace("\r\n", "\n")
 # the original data, shall be used as reference. save it somewhere, like database.
 
-linewise = data.split("\n") # there won't be "\n" in the line.
+linewise = data.split("\n")  # there won't be "\n" in the line.
 
 
-def removeDuplicates(line, chars=[" ","\t"], maxConsecutiveLength=1):
+def removeDuplicates(line, chars=[" ", "\t"], maxConsecutiveLength=1):
     for char in chars:
-        minUnallowedConsecutiveLength = maxConsecutiveLength +1
+        minUnallowedConsecutiveLength = maxConsecutiveLength + 1
         while True:
-            if char*minUnallowedConsecutiveLength in line:
-                line = line.replace(char*minUnallowedConsecutiveLength, char*maxConsecutiveLength)
+            if char * minUnallowedConsecutiveLength in line:
+                line = line.replace(
+                    char * minUnallowedConsecutiveLength, char * maxConsecutiveLength
+                )
+            else:
+                break
     return line
 
-def lstripChars(line, chars=[" ","\t"]):
+
+def lstripChars(line, chars=[" ", "\t"]):
     while True:
-        flag=False
+        flag = False
         for char in chars:
             if line.startswith(char):
                 line = line.lstrip(char)
-                flag=True
+                flag = True
         if not flag:
             break
     return line
+
 
 def standardLineCleaner(line):
     line = removeDuplicates(line)
     line = lstripChars(line)
     return line
 
+
 for lineNumber, line in enumerate(linewise):
     line_cleaned = standardLineCleaner(line)
-    print("{}:".format(lineNumber),line_cleaned)
+    print("{}:".format(lineNumber), line_cleaned)
