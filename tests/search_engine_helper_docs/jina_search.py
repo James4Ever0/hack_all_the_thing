42ac70cb5ffb7197eaec0920566c5d8221150b76
GitJournal <app@gitjournal.io>
2022-09-29 03:27:14 +0800

    [2022-09-28 19:27:14]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -9,7 +9,7 @@ from docarray import Document, DocumentArray
 
 # d = Document(uri='https://www.gutenberg.org/files/1342/1342-0.txt').load_uri_to_text()
 da = DocumentArray(
-    Document(text=elem["conv_group_merged"], line_span=elem["line_range"])
+    Document(text=elem["conv_group_merged"])
     for elem in listOfCleanedMergedConvGroupWithLineIndexMapping
 )
 da.apply(Document.embed_feature_hashing, backend="process")
