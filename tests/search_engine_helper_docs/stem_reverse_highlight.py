4e02663604d4228a9095757916367694a5fed030
GitJournal <app@gitjournal.io>
2022-09-29 23:24:17 +0800

    [2022-09-29 15:24:17]
    FILE:
    tests/search_engine_helper_docs/stem_reverse_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/stem_reverse_highlight.py
+++ tests/search_engine_helper_docs/stem_reverse_highlight.py
@@ -14,8 +14,10 @@ porterStemmer = PorterStemmer()
 def englishTextToOriginalAndStemmedWordPairs(text):
     global englishNLP, porterStemmer
     doc = englishNLP(text)
+    originalAndStemmedWordPairs = []
     for token in doc:
         text = token.text
+        
 import json
 
 with open("demo_txtai_search_results",'r') as f:
