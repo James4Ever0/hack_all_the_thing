ad899ee9080ccff00f314642a7f8ac3e327f501b
GitJournal <app@gitjournal.io>
2022-09-30 00:20:45 +0800

    [2022-09-29 16:20:45]
    FILE:
    tests/search_engine_helper_docs/whoosh_search_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_search_highlight.py
+++ tests/search_engine_helper_docs/whoosh_search_highlight.py
@@ -56,7 +56,7 @@ def index_search(dirname, search_fields, search_query):
         formatter_join_token = str(uuid.uuid4())
         # results.formatter = ListFormatter
         # it is some kind of 'html formatter', so we use BeautifulSoup
-        results.formatter = Ht
+        results.formatter = HtmlFormatter(between = formatter_join_token)
         # i mean it will join the results with some magic UUID, so you may have chance of spliting it out.
         # but to get the position is not so easy.
         # you may want the context and the exact line number.
