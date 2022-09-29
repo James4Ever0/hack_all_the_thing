69b7b018245900b0d6ba2a48cb47a3db263ac848
GitJournal <app@gitjournal.io>
2022-09-29 15:56:56 +0800

    [2022-09-29 07:56:56]
    FILE:
    tests/search_engine_helper_docs/txtai_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_search.py
+++ tests/search_engine_helper_docs/txtai_search.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
         scores = np.dot(queries, data.T).tolist()
         # uid = embeddings.similarity(query, data)[0][0]
         uids = [sorted(enumerate(score), key=lambda x: x[1], reverse=True) for score in scores]
+        # #b: 
         #                    #b #doc #data
         uid_list_top5 = uids[0][0][:5]
         # what is this shit anyway?
