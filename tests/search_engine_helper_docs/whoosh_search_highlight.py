fee922d78570a9c36606f14eee1ae5a38bacb389
GitJournal <app@gitjournal.io>
2022-09-30 00:16:52 +0800

    [2022-09-29 16:16:52]
    FILE:
    tests/search_engine_helper_docs/whoosh_search_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_search_highlight.py
+++ tests/search_engine_helper_docs/whoosh_search_highlight.py
@@ -53,6 +53,7 @@ def index_search(dirname, search_fields, search_query):
         # shall you replace the formatter.
         formatter_join_token = str(uuid.uuid4())
         # results.formatter = ListFormatter
+        results.formatter = 
         # i mean it will join the results with some magic UUID, so you may have chance of spliting it out.
         # but to get the position is not so easy.
         # you may want the context and the exact line number.
