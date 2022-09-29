44020c8e47f644efd3b6918d73b8f12fb95b374e
GitJournal <app@gitjournal.io>
2022-09-29 21:46:33 +0800

    [2022-09-29 13:46:32]
    FILE:
    tests/search_engine_helper_docs/stem_reverse_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/stem_reverse_highlight.py
+++ tests/search_engine_helper_docs/stem_reverse_highlight.py
@@ -6,6 +6,7 @@ from nltk.stem import PorterStemmer
 
 # shall we detect the language id first?
 # but this is a short excerpt. except for lingua we won't get shit.
+# just english and chinese. for other languages we don't understand so much.
 englishNLP = 
 
 with open(".json",'r') as f:
