30d51334d913973ac30fd220d35758ffc3f0ba96
GitJournal <app@gitjournal.io>
2022-09-29 20:46:54 +0800

    [2022-09-29 12:46:54]
    FILE:
    tests/search_engine_helper_docs/txtai_indexed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_indexed_search.py
+++ tests/search_engine_helper_docs/txtai_indexed_search.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         if query == "q":
             print("quitting")
             break
-        uid = embeddings.search(query, 1)[0][0]
+        uid = embeddings.search(query, 1)[0][0] # what is this thing?
 
 
         for uid, score in uid_list_top5:
