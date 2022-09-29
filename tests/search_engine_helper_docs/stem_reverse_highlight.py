3e6782e84c4e8c9418f0be9ea4f7856da24bef73
GitJournal <app@gitjournal.io>
2022-09-29 21:43:52 +0800

    [2022-09-29 13:43:52]
    FILE:
    tests/search_engine_helper_docs/stem_reverse_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/stem_reverse_highlight.py
+++ tests/search_engine_helper_docs/stem_reverse_highlight.py
@@ -1,3 +1,7 @@
+# you decide not to remove stopwords?
+# it does not affect my highlighting process anyway.
+
+import en_core_web_sm
 with open(".json",'r') as f:
     content = f.read()
     test_data = json.loads(content)
