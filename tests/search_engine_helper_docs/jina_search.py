96c93e6d3a4dd30ba124e430ac375433d3a07df7
GitJournal <app@gitjournal.io>
2022-09-29 17:18:13 +0800

    [2022-09-29 09:18:13]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -8,26 +8,23 @@ from semantic_ai_search_base_conv_with_step_charbased import (
 # query="following examples"
 from docarray import Document, DocumentArray
 
-from docarray import dataclass
-from docarray.typing import Text, JSON
+# from docarray import dataclass
+# from docarray.typing import Text, JSON
 
 
-@dataclass
-class WPExcerpt:
-    source: Text
-    text: Text
-    lineRange: JSON
-
+# @dataclass
+# class WPExcerpt:
+#     source: Text
+#     content: Text
+#     lineRange: JSON
 
 # d = Document(uri='https://www.gutenberg.org/files/1342/1342-0.txt').load_uri_to_text()
-
+# WPExcerpt(
+#         source="jq_man.log",
+#         content=elem["conv_group_merged"],  # must contain text/tags fields.
+#         lineRange=list(elem["line_range"])
 da = DocumentArray(
-    Document(
-        WPExcerpt(
-            source="jq_man.log",
-            text=elem["conv_group_merged"],  # must contain text/tags fields.
-            lineRange=list(elem["line_range"]),
-        )
+    Document(text=elem["conv_group_merged"],
     )
     for elem in listOfCleanedMergedConvGroupWithLineIndexMapping
 )
@@ -37,26 +34,25 @@ da.apply(Document.embed_feature_hashing)
 # <Document ('id', 'adjacency', '_metadata', 'embedding', 'scores', 'chunks') at 3b330837d3111c7ded9bc83bb2808f2d>
 # what is this shit?
 # query="math addition function" # not common maybe
-query = "apply to every element recursively"  # seems it does not understand this query so well.
+query = 'apply to every element recursively' # seems it does not understand this query so well.
 # we are gonna do this in txtai_search.py once again.
 
 # this feature hashing is bad. not as advanced as txtai.
-# don't know what is 'jaccard'. most like some information retrieval method.
+# don't know what is 'jaccard'. most like some
 
 q = (
-    Document(WPExcerpt(text=query, source=None, lineRange=None))
+    Document(text=query)
     .embed_feature_hashing()
     .match(da, metric="jaccard", use_scipy=True)
 )
 
 # print(q.matches[:5, ("text", "scores__jaccard__value")])
-docArray_5 = q.matches[:5, ("lineRange", "text", "scores__jaccard__value")]
+docArray_5 = q.matches[:5, ("text", 'scores__jaccard__value')]
 # two separate shit?
 mdata = list(zip(*docArray_5))
-mdata.sort(key=lambda x: x[2], reverse=True)
+mdata.sort(key=lambda x: x[1], reverse=True)
 # print(docArray_5)
 from lazero.utils.logger import sprint
-
 for hit in mdata:
     sprint(hit)
 # do we have other things?
