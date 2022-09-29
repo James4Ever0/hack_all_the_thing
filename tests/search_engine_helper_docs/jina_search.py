6bf8c22b482a5b454b043a36c58d770c7e7627d5
GitJournal <app@gitjournal.io>
2022-09-29 17:41:57 +0800

    [2022-09-29 09:41:57]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -42,7 +42,6 @@ def preproc(d: Document):
     d.embedding = embedding.transform((None, d.text, None))
     return d
 
-
 da.apply(preproc,show_progress=True, num_worker=1, backend='process')  # apply what?
 
 
