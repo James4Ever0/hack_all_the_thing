95e2f46391ff42d045213dbd9fd3933976ffeedc
GitJournal <app@gitjournal.io>
2022-09-29 20:45:28 +0800

    [2022-09-29 12:45:28]
    FILE:
    tests/search_engine_helper_docs/txtai_indexed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_indexed_search.py
+++ tests/search_engine_helper_docs/txtai_indexed_search.py
@@ -49,11 +49,6 @@ if __name__ == "__main__":
 
     embeddings.index([(uid, text, None) for uid, text in enumerate(data)])
 
-print("%-20s %s" % ("Query", "Best Match"))
-print("-" * 50)
-
-# Run an embeddings search for each query
-for query in ("feel good story", "climate change", "public health story", "war", "wildlife", "asia", "lucky", "dishonest junk"):
     # Extract uid of first result
     # search result format: (uid, score)
     uid = embeddings.search(query, 1)[0][0]
