230d7f7e858ff0a252a6de3490b6e7c768af3cd5
GitJournal <app@gitjournal.io>
2022-09-29 20:45:12 +0800

    [2022-09-29 12:45:12]
    FILE:
    tests/search_engine_helper_docs/txtai_indexed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_indexed_search.py
+++ tests/search_engine_helper_docs/txtai_indexed_search.py
@@ -46,6 +46,17 @@ if __name__ == "__main__":
             for row in progressbar.progressbar(data_source)
         ]
     )  # we need something other than this. fast info retrieval.
+
+    embeddings.index([(uid, text, None) for uid, text in enumerate(data)])
+
+print("%-20s %s" % ("Query", "Best Match"))
+print("-" * 50)
+
+# Run an embeddings search for each query
+for query in ("feel good story", "climate change", "public health story", "war", "wildlife", "asia", "lucky", "dishonest junk"):
+    # Extract uid of first result
+    # search result format: (uid, score)
+    uid = embeddings.search(query, 1)[0][0]
     # you want to use other methods?
     # embeddings.index # what is this fucking index?
     # document format: (id, data, tags)
