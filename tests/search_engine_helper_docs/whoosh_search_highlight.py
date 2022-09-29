4edcfd66a078345c0788c1bd85730a2e4e081ea3
GitJournal <app@gitjournal.io>
2022-09-30 00:06:38 +0800

    [2022-09-29 16:06:38]
    FILE:
    tests/search_engine_helper_docs/whoosh_search_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_search_highlight.py
+++ tests/search_engine_helper_docs/whoosh_search_highlight.py
@@ -52,6 +52,7 @@ def index_search(dirname, search_fields, search_query):
         print("Search Results: ")
         # shall you replace the formatter.
         # results.formatter = ListFormatter
+        # i mean it will join the results with some magic 
         # or we could directly use the highlighter without whoosh?
         for hit in results:
             # highlights = hit.highlights('content', top=5) # str. not list.
