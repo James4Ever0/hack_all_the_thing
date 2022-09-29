111ba98a038deaaccc9d2b2de2a10b0a467e11b9
GitJournal <app@gitjournal.io>
2022-09-30 00:50:21 +0800

    [2022-09-29 16:50:21]
    FILE:
    tests/search_engine_helper_docs/whoosh_search_highlight_conv_sentence_piece.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_search_highlight_conv_sentence_piece.py
+++ tests/search_engine_helper_docs/whoosh_search_highlight_conv_sentence_piece.py
@@ -23,6 +23,7 @@ from whoosh.highlight import HtmlFormatter
 
 # question: how to tokenize chinese text?
 # better search for 'whoosh 中文搜索'
+# whoosh + jieba
 schema = Schema(
     title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True),
 )
