9f75bf8e92e7cb450eaa832bb653a5793508eb93
GitJournal <app@gitjournal.io>
2022-09-30 00:37:27 +0800

    [2022-09-29 16:37:27]
    FILE:
    tests/search_engine_helper_docs/whoosh_search_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_search_highlight.py
+++ tests/search_engine_helper_docs/whoosh_search_highlight.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup as BS
 
 # remember this shit is long!
 
-# but what if we don't do 
+# but what if we don't simply import 'data'? we do the 
 
 from whoosh.fields import Schema, TEXT, ID
 from whoosh import index
