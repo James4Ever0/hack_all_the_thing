270e5b7a130a0a419a52264cbc2cdd7658b6c77c
GitJournal <app@gitjournal.io>
2022-09-29 17:45:38 +0800

    [2022-09-29 09:45:38]
    FILE:
    tests/search_engine_helper_docs/jina_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/jina_search.py
+++ tests/search_engine_helper_docs/jina_search.py
@@ -64,6 +64,7 @@ q = qd.match(da, limit=5)
 
 # print(q.matches[:5, ("text", "scores__jaccard__value")])
 docArray_5 = q.matches[:5, ("text", "tags", "scores__jaccard__value")]
+# docArray_5 = q.matches[:5, ("text", "tags", "scores__jaccard__value")]
 # two separate shit?
 mdata = list(zip(*docArray_5))
 mdata.sort(key=lambda x: x[2], reverse=True)
