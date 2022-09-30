15d823b1f8736be9cb30971df092e937402f8790
GitJournal <app@gitjournal.io>
2022-09-30 15:43:56 +0800

    [2022-09-30 07:43:56]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -3,6 +3,8 @@ from ripgrepy import Ripgrepy
 expression = "recursive"
 filepath = "/root/Desktop/works/hack_all_the_thing/tests/search_engine_helper_docs/jq_man.log"
 rg = Ripgrepy(expression, filepath) # ignore case? how?
+# but this does not include the preprocessor.
+
 result = rg.with_filename().line_number().ignore_case().json().run().as_dict
 # result = rg.with_filename().line_number().ignore_case().json().run() # this will produce list. is it right?
 
