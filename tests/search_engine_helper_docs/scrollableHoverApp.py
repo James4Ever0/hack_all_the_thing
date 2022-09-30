a4ad93b49e64e5e41c2d6335701ef983365aeb07
modifier <termux@termux.com>
2022-10-01 01:33:20 +0800

    [2022-09-30 17:33:18]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -0,0 +1,50 @@
+from rich.panel import Panel
+
+from textual.app import App
+from textual.reactive import Reactive
+from textual.widget import Widget
+from rich.text import Text
+
+# text = Text(
+#     "hello world python this is python", style="green"
+# )  # there is no style applied.
+# import os
+# text.on(click="os.system('bash less_jump_to_line.sh')") # what is this fucking toggle?
+# text.on(click="view.toggle('world')") # what is this fucking toggle?
+# text.on(click=lambda: os.system('bash less_jump_to_line.sh')) # unmarshallable. fuck.
+# text.on(click="view.toggle('side')") 
+
+class Hover(Widget):
+
+    mouse_over = Reactive(False)
+
+    def render(self) -> Panel:
+        text = Text(self.name)
+        return Panel(text, style=("on red" if self.mouse_over else ""))
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
+    
+    def on_click(self):
+        # if self.name == "widget 1":
+        #     # import os
+        #     # command = 'bash less_jump_to_line.sh'
+        #     # os.system(command) # does not work properly! fuck.
+        # else:
+        print("CLICKED {}".format(self.name))
+
+
+
+class HoverApp(App):
+    """Demonstrates custom widgets"""
+
+    async def on_mount(self) -> None:
+        hovers = (Hover("widget {}".format(index)) for index in range(3))
+        await self.view.dock(*hovers, edge="top",name='side') #WTF?
+        # here we got the view.
+
+
+HoverApp.run()
