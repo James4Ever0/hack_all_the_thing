1c25f14bfa34c56701fbbc941fb23610a171e829
GitJournal <app@gitjournal.io>
2022-09-30 23:18:54 +0800

    [2022-09-30 15:18:54]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -97,7 +97,7 @@ class MyApp(App):
 
         lineNumber2 = sum(equivalentLineCountPerLine[:lineNumber])
         # lineNumber2 = max(0, lineNumber2-center)
-        context = 3
+        context = 0
         lineNumber2 = max(0, lineNumber2-1-context) # minus 1 to get the exact line location.
         # you want more context?
         # total_sum =
