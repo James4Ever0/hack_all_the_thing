21603eb64ba5605ec414e7f7929ce4954acf6953
GitJournal <app@gitjournal.io>
2022-09-30 17:01:05 +0800

    [2022-09-30 09:01:05]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    tests/search_engine_helper_docs/textual.log
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -24,8 +24,8 @@ class MyApp(App):
     async def on_mount(self) -> None:
         await self.view.dock(self.body, edge="top")
         await self.body.update(self.contentText)
-        scrollTo = ScrollTo(self.body,0,lineNumber)
-        await self.body.handle_scroll_to(scrollTo)
+        # scrollTo = ScrollTo(self.body,0,lineNumber)
+        # await self.body.handle_scroll_to(scrollTo)
         # self.body.scroll_in_to_view(lineNumber)
         # self.body.target_y = lineNumber
         # self.body.y = lineNumber
