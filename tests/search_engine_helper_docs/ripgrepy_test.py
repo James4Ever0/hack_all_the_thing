532caadd4962f34b4fc7cb6ab4de634bc05e6c03
GitJournal <app@gitjournal.io>
2022-09-30 15:34:18 +0800

    [2022-09-30 07:34:18]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -1,4 +1,4 @@
-import ripgrepy import Ripgrepy
+from ripgrepy import Ripgrepy
 # The Ripgrepy class takes two arguments. The regex to search for and the folder path to search in
-
+expression = 
 rg = Ripgrepy(expression, fipepath)
