2fc103d312a6ec44d517619de194f5f20ab4d3de
GitJournal <app@gitjournal.io>
2022-09-30 18:32:50 +0800

    [2022-09-30 10:32:50]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    tests/search_engine_helper_docs/textual.log
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -37,6 +37,7 @@ class MyApp(App):
         self.body.set_y(lineNumber)
         with self.body.window.console.capture() as capture:
             self.body.window.refresh()
+        print(capture.get())
         # self.body.animate("y", lineNumber,speed=lineNumber*3, easing="out_cubic")
         # await self.body.watch_y(lineNumber)
 
