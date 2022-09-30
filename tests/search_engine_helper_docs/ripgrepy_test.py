64b0d1dd5771bc106dab66e055a60e6603f4f15d
GitJournal <app@gitjournal.io>
2022-09-30 15:35:15 +0800

    [2022-09-30 07:35:15]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -1,4 +1,4 @@
 from ripgrepy import Ripgrepy
 # The Ripgrepy class takes two arguments. The regex to search for and the folder path to search in
-expression = 
+expression = "recursive"
 rg = Ripgrepy(expression, filepath) # ignore case? how?
