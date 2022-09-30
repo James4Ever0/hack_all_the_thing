d95e12078aa77c231fb94bcc32c4c5a0a57b9d01
GitJournal <app@gitjournal.io>
2022-09-30 15:33:54 +0800

    [2022-09-30 07:33:54]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -1,3 +1,7 @@
 import ripgrepy
 
 # example?
+from ripgrepy import Ripgrepy
+# The Ripgrepy class takes two arguments. The regex to search for and the folder path to search in
+
+rg = Ripgrepy('he[l]{2}o', '/some/path/to/files')
