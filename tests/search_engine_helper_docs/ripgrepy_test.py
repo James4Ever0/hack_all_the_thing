75d4176208153eb9e9e671cf217b3499bcbb5157
GitJournal <app@gitjournal.io>
2022-09-30 15:37:39 +0800

    [2022-09-30 07:37:39]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -4,3 +4,4 @@ expression = "recursive"
 filepath = "/root/Desktop/works/hack_all_the_thing/tests/search_engine_helper_docs/jq_man.log"
 rg = Ripgrepy(expression, filepath) # ignore case? how?
 result = rg.with_filename().line_number().ignore_case().run()
+
