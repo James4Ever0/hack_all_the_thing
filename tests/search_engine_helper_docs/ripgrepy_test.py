fd0aa67334db42ca2e4dca7f19fb4ce92c5450a9
GitJournal <app@gitjournal.io>
2022-09-30 15:43:01 +0800

    [2022-09-30 07:43:01]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -4,7 +4,7 @@ expression = "recursive"
 filepath = "/root/Desktop/works/hack_all_the_thing/tests/search_engine_helper_docs/jq_man.log"
 rg = Ripgrepy(expression, filepath) # ignore case? how?
 result = rg.with_filename().line_number().ignore_case().json().run().as_dict
-# result = rg.with_filename().line_number().ignore_case().json().run() # this will produce list.
+# result = rg.with_filename().line_number().ignore_case().json().run() # this will produce list. is it right?
 
 import rich
 
