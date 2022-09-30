d5d92c70552171e043e38bd7de9474c8a6e32e9b
GitJournal <app@gitjournal.io>
2022-09-30 19:42:30 +0800

    [2022-09-30 11:42:30]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -24,7 +24,7 @@ class MyApp(App):
         # [('name','ScrollView#1')]
         console=Console()
         with console.capture() as capture:
-            renderableLayout = self.body.window.__rich__() #renderable!
+            renderableLayout = self.body.window.__rich_console__(console) #renderable!
             console.print(renderableLayout)
         result=capture.get()
         # results_list = [x for x in results]
