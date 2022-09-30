b8f854bb2ebcb04207f54c3db026f00f9cd4ea63
GitJournal <app@gitjournal.io>
2022-09-30 16:54:00 +0800

    [2022-09-30 08:54:00]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -24,6 +24,6 @@ class MyApp(App):
         await self.view.dock(self.body, edge="top")
         await self.body.update(self.contentText)
         # self.body.scroll_in_to_view(lineNumber)
-        # self.body.scroll_y = lineNumber
+        # self.body.target_y = lineNumber
     
 MyApp.run(title="Code Viewer", log="textual.log")
