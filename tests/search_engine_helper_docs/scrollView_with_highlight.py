7de81653bad654a27b1beb310e1c4c869209063a
GitJournal <app@gitjournal.io>
2022-09-30 17:52:59 +0800

    [2022-09-30 09:52:59]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -29,10 +29,9 @@ class MyApp(App):
         await self.body.update(self.contentText)
         # self.body.y= lineNumber
         # self.body.target_y = lineNumber
-        scrollTo = ScrollTo(self.body.vscroll, 0, lineNumber)
+        # scrollTo = ScrollTo(self.body.vscroll, 0, lineNumber)
         # await self.body.emit(scrollTo)
-        await.self.body.y =
-        # self.body.animate("y", lineNumber, speed=lineNumber*3, easing="out_cubic")
+        self.body.animate("y", lineNumber, duration=0, easing="out_cubic")
         # self.body.set_y(lineNumber)
         # await self.body.watch_y(lineNumber)
 
