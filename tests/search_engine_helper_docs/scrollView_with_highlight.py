b036d885de4ee922190a56020c744397ae010133
GitJournal <app@gitjournal.io>
2022-09-30 16:37:13 +0800

    [2022-09-30 08:37:13]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -1,5 +1,6 @@
 lineNumber = 2268
 keywords = set(['recursive'])
+filepath ='/root/Desktop/works/hack_all_the_thing/tests/search_engine_helper_docs/jq_man.log'
 
 from textual.widgets import ScrollView
 view = ScrollView()
