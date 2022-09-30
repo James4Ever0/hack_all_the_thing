183dad1e8ea947746278884b8318a9617e5a00ab
GitJournal <app@gitjournal.io>
2022-09-30 23:43:49 +0800

    [2022-09-30 15:43:49]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -86,7 +86,7 @@ class MyApp(App):
         # self.set_interval(5, self.refresh)
         await self.view.dock(self.body, edge="top")
         await self.body.update(self.contentText)
-        lineNumber2 = 
+        self.
 
     def jumpToEquivalentLineNumber(self,content_line_char_count,lineNumber):
         size = os.get_terminal_size()
