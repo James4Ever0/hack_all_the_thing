5e0e11486651f1599f2c5216d6bcade7b3c8c24c
modifier <termux@termux.com>
2022-10-01 00:56:37 +0800

    [2022-09-30 16:56:36]
    FILE:
    tests/search_engine_helper_docs/fix_scroll_view.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/fix_scroll_view.sh
+++ tests/search_engine_helper_docs/fix_scroll_view.sh
@@ -1,1 +1,2 @@
 cp _scroll_view.py /usr/local/lib/python3.9/dist-packages/textual/widgets/_scroll_view.py
+cp _scroll_view.py /data/data/com.termux/files/usr/lib/python3.10/site-packages/textual/_scroll_view.py
