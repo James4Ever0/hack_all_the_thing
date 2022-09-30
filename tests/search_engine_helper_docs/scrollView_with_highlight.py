ba8f1ba41a92274aaace734492505418833c8582
modifier <termux@termux.com>
2022-10-01 01:41:40 +0800

    [2022-09-30 17:41:39]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    tests/search_engine_helper_docs/textual.log
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -114,7 +114,7 @@ class MyApp(App):
 
         lineNumber2 = sum(equivalentLineCountPerLine[:lineNumber])
         # lineNumber2 = max(0, lineNumber2-center)
-        context = 0
+        context = lines
         lineNumber2 = max(
             0, lineNumber2 - 1 - context
         )  # minus 1 to get the exact line location.
