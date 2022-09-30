1ce5807377c8b42089187d66fa19dc128a57155d
modifier <termux@termux.com>
2022-10-01 01:45:49 +0800

    [2022-09-30 17:45:48]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -116,7 +116,7 @@ class MyApp(App):
         # lineNumber2 = max(0, lineNumber2-center)
         context = 0
         lineNumber2 = max(
-            0, lineNumber2 - 1 - context
+            0, lineNumber2 - 1 - context+lines
         )  # minus 1 to get the exact line location.
         self.body.set_y(lineNumber2)
 
