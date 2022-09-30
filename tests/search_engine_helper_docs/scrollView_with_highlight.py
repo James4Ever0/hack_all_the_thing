025ad102d977b5314634937b4a33e567a7935222
GitJournal <app@gitjournal.io>
2022-09-30 21:40:03 +0800

    [2022-09-30 13:40:03]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -71,7 +71,7 @@ class MyApp(App):
         import os
 
         size = os.get_terminal_size()
-        msize = self.body.window.
+        msize = self.body.size
         print("WINDOW SIZE:", msize) # not right!
         columns, lines = size.columns, size.lines
         print("COLUMNS:",columns)
