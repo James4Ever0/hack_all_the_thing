4fc26d4c0f48289cf02c9f458295ebb9dfb2155e
GitJournal <app@gitjournal.io>
2022-09-30 18:58:48 +0800

    [2022-09-30 10:58:48]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -16,7 +16,7 @@ from textual.app import App
 class MyApp(App):
     # how to let me copy the text inslde? fuck?
     async def on_load(self) -> None:
-        await self.bind('c','Copy')
+        await self.bind('c',action,'Copy')
         self.body = ScrollView()
         with open(filepath, 'r') as f:
             content = f.read()
