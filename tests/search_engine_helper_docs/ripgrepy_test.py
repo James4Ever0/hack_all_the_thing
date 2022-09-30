6bdb0300ad14b648bfe3686de38d5a99263525ec
GitJournal <app@gitjournal.io>
2022-09-30 16:18:43 +0800

    [2022-09-30 08:18:43]
    FILE:
    tests/search_engine_helper_docs/ripgrepy_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/ripgrepy_test.py
+++ tests/search_engine_helper_docs/ripgrepy_test.py
@@ -15,7 +15,7 @@ for elem in result:
     path_text = data['path']['text']
     line_text = data['lines']['text']
     line_number = data['lines']['line_number']
-    submatches = data[]
+    submatches = data['submatches']
 # rich.print(result)
 #     {
 #         'type': 'match',
