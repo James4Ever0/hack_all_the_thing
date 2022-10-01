9d6e4a9c348cb2cfb64ea9440f408fe584dac26b
GitJournal <app@gitjournal.io>
2022-10-01 16:51:09 +0800

    [2022-10-01 08:51:09]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -65,7 +65,9 @@ class HoverApp(App):
 
         # self.renderableHovers = Table()
         for index in range(3): # this is bad. these things are not clickable.
-            self.layout.add()
+            self.layout.add(        #         Hover(
+        #             "widget {}".format(index),
+        #         ))
         #     self.renderableHovers.add_row(
         #         Hover(
         #             "widget {}".format(index),
