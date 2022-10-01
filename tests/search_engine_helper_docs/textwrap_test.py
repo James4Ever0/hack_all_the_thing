3b60807b3ed2a8f9e10578d77a8939570eb41812
GitJournal <app@gitjournal.io>
2022-10-01 13:06:48 +0800

    [2022-10-01 05:06:48]
    FILE:
    tests/search_engine_helper_docs/textwrap_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/search_engine_helper_docs/textwrap_test.py
@@ -0,0 +1,11 @@
+
+
+def wrapText(textList,width): # the width is col-1
+    content_line_char_count = []
+    wrapped_lines = []
+    for text in textList:
+        lines = textwrap.wrap(text,width=width)
+        wrapped_lines.extend(lines)
+        lineCount = len(lines)
+        content_line_char_count.append(lineCount)
+    return wrapped_lines, content_line_char_count
