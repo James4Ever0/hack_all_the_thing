eace9056dac32ac6a87ec1508efe90054bb8f1e8
GitJournal <app@gitjournal.io>
2022-10-01 18:11:40 +0800

    [2022-10-01 10:11:40]
    FILE:
    tests/search_engine_helper_docs/textual_input_search_box.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_input_search_box.py
+++ tests/search_engine_helper_docs/textual_input_search_box.py
@@ -1,5 +1,5 @@
 # aim to build some search box here. dock this.
 # are you sure you will listen to the key events? let's try to toggle this thing via key 's', retrieve input via 'enter' key?
 
-# make it center.
+# make it centered.
 
