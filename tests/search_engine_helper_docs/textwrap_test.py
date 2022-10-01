01e69d4632d546085bee41265509bd10e76410c3
GitJournal <app@gitjournal.io>
2022-10-01 13:10:12 +0800

    [2022-10-01 05:10:12]
    FILE:
    tests/search_engine_helper_docs/textwrap_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textwrap_test.py
+++ tests/search_engine_helper_docs/textwrap_test.py
@@ -1,12 +1,13 @@
 import textwrap
 
-textList = ['123','1234']*2000
+textList = ["123", "1234"] * 2000
 
-def wrapText(textList,width): # the width is col-1
+
+def wrapText(textList, width):  # the width is col-1
     content_line_char_count = []
     wrapped_lines = []
     for text in textList:
-        lines = textwrap.wrap(text,width=width)
+        lines = textwrap.wrap(text, width=width)
         wrapped_lines.extend(lines)
         lineCount = len(lines)
         content_line_char_count.append(lineCount)
