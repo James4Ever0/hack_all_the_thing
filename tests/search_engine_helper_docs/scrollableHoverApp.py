dcbf48802bd507e9a93e8149ae31eb5feeda4be1
GitJournal <app@gitjournal.io>
2022-10-01 17:45:53 +0800

    [2022-10-01 09:45:53]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -72,7 +72,7 @@ class HoverApp(App):
         # )
         self.mainViewer = ScrollView()  # with name or not? you need keywords.
         # hoverRenderable = self.hovers
-        self.mainViewer.update(Text)
+        self.mainViewer.update(Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '))
         # self.renderableHovers = Table()
         self.scrollableHovers = ListViewUo(
             [
