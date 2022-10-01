41dd5fa97a09a7274cf5abce7e0e41cb2c84723a
GitJournal <app@gitjournal.io>
2022-10-01 10:03:49 +0800

    [2022-10-01 02:03:49]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -13,6 +13,7 @@ filepath = (
 )
 
 # implement soft/hard wrap method yourself, or the scroll view will get tricky.
+import textwrap
 def 
 # filepath = "test.txt"  # col: 108
 # now check the layout?
