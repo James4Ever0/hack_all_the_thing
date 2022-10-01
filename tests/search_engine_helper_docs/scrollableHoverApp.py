f6d50b4954a4588eab337a4f81c45549f6789d90
GitJournal <app@gitjournal.io>
2022-10-01 18:08:54 +0800

    [2022-10-01 10:08:54]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -71,8 +71,8 @@ class HoverApp(App):
     async def on_key(self, event):
         # self.console.bell()
         key = event.key
-        print("KEYCODE: {}".format(key))
-        breakpoint()
+        print("KEYCODE: {}".format(key)) # we want something like 'ESC'
+        breakpoint() 
         key_lower = key.lower()
         # handle input elsewhere?
         # when text field is focused, we do not do shit.
