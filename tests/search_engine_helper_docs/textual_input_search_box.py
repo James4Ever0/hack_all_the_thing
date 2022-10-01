5238ab2bc4767bb6b7da70713c056090dcd6f1c7
GitJournal <app@gitjournal.io>
2022-10-01 21:45:44 +0800

    [2022-10-01 13:45:43]
    FILE:
    tests/search_engine_helper_docs/textual_input_search_box.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_input_search_box.py
+++ tests/search_engine_helper_docs/textual_input_search_box.py
@@ -21,8 +21,9 @@ class HoverApp(App):
     async def action_searchToggle(self):
         await self.view.action_toggle("search")
         if self.mainInput.visible:
-            self.mainInput.focus()
-        await self.view.focus() # deactivate the search field?
+            await self.mainInput.focus()
+        else:
+            await self.view.focus() # deactivate the search field?
 
 
     async def action_submit(self):
