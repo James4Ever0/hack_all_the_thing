416d49fa2580647cd8729376ada6f5016dddc6cb
GitJournal <app@gitjournal.io>
2022-10-01 17:43:47 +0800

    [2022-10-01 09:43:47]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -76,7 +76,7 @@ class HoverApp(App):
             [
                 # this is bad. these things are not clickable.
                 Hover(
-                    "widget {}".format(index),
+                    "widget {}".format(index),onClick = 
                 )
                 for index in range(30)
             ]
