b5c800a0416f03b2844c48bad6219cfbac0f75cd
GitJournal <app@gitjournal.io>
2022-10-01 18:06:28 +0800

    [2022-10-01 10:06:27]
    FILE:
    tests/search_engine_helper_docs/textual_input_search_box.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_input_search_box.py
+++ tests/search_engine_helper_docs/textual_input_search_box.py
@@ -1,2 +1,2 @@
 # aim to build some search box here. dock this.
-# are you sure you will listen to the key events? let's try to toggle this thing via key 's'
+# are you sure you will listen to the key events? let's try to toggle this thing via key 's', retrieve input via 'enter' key?
