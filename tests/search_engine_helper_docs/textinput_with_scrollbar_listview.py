50b8913ff02f7658abd200c6c5bfdec9938712ba
modifier <termux@termux.com>
2022-10-02 02:18:24 +0800

    [2022-10-01 18:18:24]
    FILE:
    tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
+++ tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
@@ -48,8 +48,9 @@ class MyApp(App):
             await self.jumpScrollView()
         elif key == 's':
             await self.focusSearchView()
-  async def action_clearSearchView(self):
-        self.mainInput.value=''    async def focusSearchView(self):
+    async def action_clearSearchView(self):
+        self.mainInput.value=''
+    async def focusSearchView(self):
         #await self.view.action_toggle('search')
         if not self.mainInput.visible:
             await self.view.action_toggle('search')
