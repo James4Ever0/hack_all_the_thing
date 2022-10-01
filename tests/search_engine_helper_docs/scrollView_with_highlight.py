2b63c1911ec2cab6264dbf333b09ddb6a9698325
GitJournal <app@gitjournal.io>
2022-10-01 14:40:05 +0800

    [2022-10-01 06:40:05]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -6,9 +6,7 @@ import os
 import math
 
 keywords = set(["recursive"])
-filepath = (
-    "./jq_man.log"
-)
+filepath = "./jq_man.log"
 
 # implement soft/hard wrap method yourself, or the scroll view will get tricky.
 import textwrap
@@ -42,35 +40,42 @@ with open(filepath, "r") as f:
 
 class MyApp(App):
     # how to let me copy the text inslde? fuck?
-    index=0
-    readerName="ScrollFileReader"
-    content_line_char_count= []
-    lineNumbers = [2268]
+    index = 0
+    readerName = "ScrollFileReader"
+    content_line_char_count = []
+    lineNumbers = [2235] # adjusted.
+    # lineNumbers = [2268]
     # lineNumbers=[2923,2878,0,5] # we test this on android first.
-    def wrapText(self,textList,width): # the width is col-1
+    def wrapText(self, textList, width):  # the width is col-1
         content_line_char_count = []
         wrapped_lines = []
         for text in textList:
-            lines = textwrap.wrap(text,width=width)
-            wrapped_lines.extend(lines)
+            lines = textwrap.wrap(text, width=width)
             lineCount = len(lines)
+            if lineCount == 0:
+                lines = [""]
+                lineCount = 1
             content_line_char_count.append(lineCount)
+            wrapped_lines.extend(lines)
         return wrapped_lines, content_line_char_count
+
     async def on_key(self, event):
         # self.console.bell()
         key = event.key
         if key == "t":
             await self.toggleScrollView()
-        elif key == 'j':
+        elif key == "j":
             await self.jumpScrollView()
 
     async def jumpScrollView(self):
         # jump to next candidate.
         # will it work for hidden ScrollView
         if self.body.visible:
-            self.index+=1
-            self.index%=len(self.lineNumbers)
-            self.jumpToEquivalentLineNumber(self.content_line_char_count,self.lineNumbers[self.index])
+            self.index += 1
+            self.index %= len(self.lineNumbers)
+            self.jumpToEquivalentLineNumber(
+                self.content_line_char_count, self.lineNumbers[self.index]
+            )
 
     async def toggleScrollView(self):
         # results=self.body.__rich_repr__() # generator!
@@ -98,14 +103,18 @@ class MyApp(App):
         self.body = ScrollView(name=self.readerName)
 
         from rich.text import Text
+
         size = os.get_terminal_size()
         columns, lines = size.columns, size.lines
         textList = content.split("\n")
-        wrapped_lines, self.content_line_char_count = self.wrapText(textList, columns-1)
+        wrapped_lines, self.content_line_char_count = self.wrapText(
+            textList, columns - 1
+        )
         processed_text = "\n".join(wrapped_lines)
 
         self.contentText = Text(processed_text)
-        highlightLine = "       erator will be efficient. In the example below the recursive call by _range\n"
+        # highlightLine = "       erator will be efficient. In the example below the recursive call by _range\n"
+        highlightLine = "will be efficient. In the example below the recursive call by _range to itself"
         highlightWord = "recursive"  # maybe not so right.
         self.contentText.highlight_words([highlightLine], style="black on red")
         self.contentText.highlight_words([highlightWord], style="yellow on red")
@@ -118,10 +127,12 @@ class MyApp(App):
         # register something on window size change?
 
         await self.body.update(self.contentText)
-        self.jumpToEquivalentLineNumber(self.content_line_char_count,self.lineNumbers[self.index])
+        self.jumpToEquivalentLineNumber(
+            self.content_line_char_count, self.lineNumbers[self.index]
+        )
 
-    def jumpToEquivalentLineNumber(self,content_line_char_count,lineNumber):
-        # size = os.get_terminal_size()
+    def jumpToEquivalentLineNumber(self, content_line_char_count, lineNumber):
+        size = os.get_terminal_size()
         # msize = self.body.window.layout.width# it is totally not right!
         # the width is zero. means adaptive?
         # print("WINDOW SIZE:", msize) # not right!
@@ -133,20 +144,25 @@ class MyApp(App):
 
         # equivalentLineCountPerLine = [
         #     max(
-        #         1, math.ceil(length / (columns-1))
+        # #         1, math.ceil(length / (columns-1))
+        #         1, math.ceil(length)
         #     )  # that's because of the scrollbar taking 1 extra column.
         #     for length in content_line_char_count
         # ]
 
         lineNumber2 = sum(equivalentLineCountPerLine[:lineNumber])
         # lineNumber2 = max(0, lineNumber2-center)
-        context = 0
+        context = 4 # true context, no extra bullshit. -> real line on rendered result
         lineNumber2 = max(
-            0, lineNumber2-context
+            0, lineNumber2 - 1 - context
         )  # minus 1 to get the exact line location.
-        print("LINE NUMBER:", lineNumber)
-        print("LINE NUMBER 2:", lineNumber2)
-        breakpoint()
-        self.body.set_y(lineNumber2)
+
+        # print("LINE NUMBER:", lineNumber)
+        # print("LINE NUMBER 2:", lineNumber2)
+        # print("LEN:", len(equivalentLineCountPerLine))
+        # breakpoint()
+        # self.body.set_y(lineNumber2)
+        self.body.set_y(lineNumber2) # how about let's not alter this thing?
+
 
 MyApp.run(title="Code Viewer", log="textual.log")
