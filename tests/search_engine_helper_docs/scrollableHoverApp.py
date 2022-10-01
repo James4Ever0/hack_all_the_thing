88845f88b2b6a8303a3aa6cc4cfce49e0737b795
GitJournal <app@gitjournal.io>
2022-10-01 16:03:45 +0800

    [2022-10-01 08:03:45]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -44,7 +44,7 @@ class HoverApp(App):
 
     async def on_mount(self) -> None:
         hovers = (Hover("widget {}".format(index)) for index in range(3))
-        scrollableHovers = 
+        scrollableHovers = ScrollView
         await self.view.dock(*hovers, edge="top",name='side') #WTF?
         # here we got the view.
 
