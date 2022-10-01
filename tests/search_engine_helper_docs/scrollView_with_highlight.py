ded1ffb9ce36115f0070ad06b48268f0fafdcbb4
GitJournal <app@gitjournal.io>
2022-10-01 10:10:07 +0800

    [2022-10-01 02:10:07]
    FILE:
    tests/search_engine_helper_docs/scrollView_with_highlight.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollView_with_highlight.py
+++ tests/search_engine_helper_docs/scrollView_with_highlight.py
@@ -15,7 +15,7 @@ filepath = (
 # implement soft/hard wrap method yourself, or the scroll view will get tricky.
 import textwrap
 def wrapText(textList,width):
-    
+content_
     for text in  textList:
         lines = textwrap.wrap(text,width=width)
         lineCount = len(lines)
