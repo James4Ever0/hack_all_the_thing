4b74bce843c61a88928ca39a688fadc67082e1a7
GitJournal <app@gitjournal.io>
2022-10-01 23:08:45 +0800

    [2022-10-01 15:08:45]
    FILE:
    tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
+++ tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
@@ -82,7 +82,7 @@ class MyApp(App):
 
     async def on_mount(self) -> None:
         self.mainInput = TextInput(
-            name="search", placeholder="enter your query", title="search input", height=4
+            name="search", placeholder="enter your query", title="search input"
         )
         await self.view.dock(self.mainInput, self.body,edge="top")
 
