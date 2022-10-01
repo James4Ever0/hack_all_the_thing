159d71f1507cdb4917df8de155b1d406f979846c
GitJournal <app@gitjournal.io>
2022-10-01 22:34:22 +0800

    [2022-10-01 14:34:22]
    FILE:
    tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
+++ tests/search_engine_helper_docs/textinput_with_scrollbar_listview.py
@@ -113,8 +113,6 @@ class MyApp(App):
         await self.bind("enter", "submit", "Submit")
         await self.bind("ctrl+s", "searchToggle", "searchToggle")
         await self.bind("escape", "reset_focus", show=False)
-        # we want you to hide the thing!
-    async def on_load(self) -> None:
         # action = 'copyScrollView()'
         self.body = ScrollView(name=self.readerName)
 
