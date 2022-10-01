75628f4a290acde9f7e025cb69ed306a003a6ce9
GitJournal <app@gitjournal.io>
2022-10-01 17:45:25 +0800

    [2022-10-01 09:45:25]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -9,7 +9,7 @@ from textual.reactive import Reactive
 from textual.widget import Widget
 from rich.text import Text
 
-from textual.scroll
+from textual.widgets import ScrollView
 
 # text = Text(
 #     "hello world python this is python", style="green"
