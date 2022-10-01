2f1dc131d00671a3cd890f2382e84e1779b22f66
GitJournal <app@gitjournal.io>
2022-10-01 16:33:26 +0800

    [2022-10-01 08:33:26]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp.py
+++ tests/search_engine_helper_docs/scrollableHoverApp.py
@@ -5,7 +5,7 @@ from textual.reactive import Reactive
 from textual.widget import Widget
 from rich.text import Text
 # from rich.table import Table
-from textual.layout import VerticalLayout
+from textual.layout
 # text = Text(
 #     "hello world python this is python", style="green"
 # )  # there is no style applied.
