450d516e41a1d11304f4f1b4dc88920fdfa7e13f
GitJournal <app@gitjournal.io>
2022-10-02 13:14:52 +0800

    [2022-10-02 05:14:52]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
+++ tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
@@ -83,7 +83,7 @@ class HoverApp(App):
             await self.alterListView()
     
     async def alterListView(self):
-        self.scrollableHovers.update() # what should we update?
+        self.scrollableHovers.update(content) # what should we update?
 
     async def on_mount(self) -> None:
         # self.hovers = (
