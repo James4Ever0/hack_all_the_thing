ee3703715fb53e28b2a31fbd071cef9dea0ab4cd
GitJournal <app@gitjournal.io>
2022-10-02 20:21:48 +0800

    [2022-10-02 12:21:48]
    FILE:
    tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
+++ tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
@@ -13,12 +13,12 @@ from lazero.utils.logger import sprint
 # this is a search application! damn it.
 
 # we still got some unfixed hyphen problems. damn.
-from semantic_ai_search_base_conv_with_step_charbased import (
-    listOfCleanedMergedConvGroupWithLineIndexMapping,
-) # recursive every element apply
-# from semantic_ai_chinese_english import (
+# from semantic_ai_search_base_conv_with_step_charbased import (
 #     listOfCleanedMergedConvGroupWithLineIndexMapping,
-# ) # video language recognition
+# ) # recursive every element apply
+from semantic_ai_chinese_english import (
+    listOfCleanedMergedConvGroupWithLineIndexMapping,
+) # video language recognition
 
 data_source = [
     elem["conv_group_merged"]
