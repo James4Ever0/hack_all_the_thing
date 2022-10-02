189e5efcd1ec9bd353f8a17c6d673fca2a9297de
GitJournal <app@gitjournal.io>
2022-10-02 15:38:24 +0800

    [2022-10-02 07:38:24]
    FILE:
    tests/search_engine_helper_docs/textual_screenshot.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_screenshot.py
+++ tests/search_engine_helper_docs/textual_screenshot.py
@@ -10,7 +10,7 @@ class MyApp(App):
         await self.bind("y", "screenshot", "Screenshot")
     async def on_mount(self):
         self.body = ScrollView()
-        self.body.update()
+        self.body.update(Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'))
         await self.view.dock(self.body, edge="top")
 
 
