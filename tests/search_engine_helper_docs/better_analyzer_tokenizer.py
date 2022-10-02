299850a135e601b64fb8b676399a0248528082d5
GitJournal <app@gitjournal.io>
2022-10-03 03:26:08 +0800

    [2022-10-02 19:26:08]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -1,12 +1,11 @@
 samples = [
-    "content: ) ○ 36 (cyan) ○ 37 (white) BUGS Presumably. Report them or discuss them at: https://github", # conventional english
-    "https://github.com/stedolan/jq/issues", # github link
-    """"""
-寻找潜在爆款话题 标签
+    "content: ) ○ 36 (cyan) ○ 37 (white) BUGS Presumably. Report them or discuss them at: https://github",  # conventional english
+    "https://github.com/stedolan/jq/issues",  # github link
+    """寻找潜在爆款话题 标签
 
 快排参数 上首页
 https://github.com/sopify-bot/seo
 
 分为主动点击 换IP点击 
-以及优化自身关键词 被动优化两种方式""" # chinese with english
-    ]
+以及优化自身关键词 被动优化两种方式""",  # chinese with english
+]
