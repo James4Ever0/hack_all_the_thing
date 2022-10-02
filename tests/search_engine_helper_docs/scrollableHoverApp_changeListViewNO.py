db3dbf1dddad857311a1da01a31c4dae8a2415af
GitJournal <app@gitjournal.io>
2022-10-02 13:58:56 +0800

    [2022-10-02 05:58:56]
    FILE:
    tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
+++ tests/search_engine_helper_docs/scrollableHoverApp_changeListViewNO.py
@@ -100,6 +100,7 @@ class HoverApp(App):
                 for index in range(30)
             ]
         )  # what should we update?
+        self.view.toggle()
         await self.view.dock(self.scrollableHovers, edge="top", name="side")  # WTF?
 
     async def on_mount(self) -> None:
