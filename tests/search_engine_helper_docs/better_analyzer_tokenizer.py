2af3db9394790216d5072aa04eb820601752c9bd
GitJournal <app@gitjournal.io>
2022-10-03 03:24:38 +0800

    [2022-10-02 19:24:38]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -1,4 +1,4 @@
 samples = [
     "content: ) ○ 36 (cyan) ○ 37 (white) BUGS Presumably. Report them or discuss them at: https://github",
-    "           https://github.com/stedolan/jq/issues
-"]
+    "https://github.com/stedolan/jq/issues",
+    ""]
