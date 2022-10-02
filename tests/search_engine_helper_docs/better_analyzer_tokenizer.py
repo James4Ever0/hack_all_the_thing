fa83744423213f28edfbcc819f10425bc50d7a67
GitJournal <app@gitjournal.io>
2022-10-03 03:32:59 +0800

    [2022-10-02 19:32:59]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -16,5 +16,5 @@ https://github.com/sopify-bot/seo
 # query = "math addition operation" # strange enbedding!
 # query="following examples"
 from docarray import Document, DocumentArray
-""",  # wordninja
+""",  # wordninja, glued 
 ]
