cadbd162425ec493683def332c2cadef4442e6bc
GitJournal <app@gitjournal.io>
2022-10-02 21:46:57 +0800

    [2022-10-02 13:46:57]
    FILE:
    tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
+++ tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
@@ -11,6 +11,4 @@ ann_query_embedding = np.fromfile("ann_query_embedding.txt",dtype=np.float32).re
 # it still needs gpu.
 # reshape this thing.
 
-limit = 3
-result = embeddings.ann.search(ann_query_embedding, limit) # how long does it take for you to load all these stuff?
-print(result)
+limit = 5
