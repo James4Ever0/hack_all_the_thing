0470979396a76266e106052008c458e388f583d7
GitJournal <app@gitjournal.io>
2022-10-02 15:21:20 +0800

    [2022-10-02 07:21:20]
    FILE:
    tests/search_engine_helper_docs/textual_screenshot.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/search_engine_helper_docs/textual_screenshot.py
@@ -0,0 +1,11 @@
+from textual.app import App
+from rich.console import Console
+
+class MyApp(App):
+    async def on_load(self) -> None:
+        await self.bind("s", "screenshot", "Screenshot")
+
+    def action_screenshot(self) -> None:
+        console = Console(record=True)
+        console.print(self)
+        console.save_svg("screenshot.svg", title="MyTitle")
