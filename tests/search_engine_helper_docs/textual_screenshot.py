c02e8ac79248b9dc5d2ef69e2537402a43a27b74
GitJournal <app@gitjournal.io>
2022-10-02 15:21:34 +0800

    [2022-10-02 07:21:34]
    FILE:
    tests/search_engine_helper_docs/textual_screenshot.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_screenshot.py
+++ tests/search_engine_helper_docs/textual_screenshot.py
@@ -3,7 +3,7 @@ from rich.console import Console
 
 class MyApp(App):
     async def on_load(self) -> None:
-        await self.bind("s", "screenshot", "Screenshot")
+        await self.bind("y", "screenshot", "Screenshot")
 
     def action_screenshot(self) -> None:
         console = Console(record=True)
