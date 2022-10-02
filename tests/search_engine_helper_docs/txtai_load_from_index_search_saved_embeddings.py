15cd1c345e041da7fe312281844a7d297b8b3466
GitJournal <app@gitjournal.io>
2022-10-02 21:29:52 +0800

    [2022-10-02 13:29:52]
    FILE:
    tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
+++ tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
@@ -4,7 +4,7 @@ from txtai.embeddings import Embeddings
 embeddings = Embeddings()
 embeddings.load("./multilingual_index_demo")
 
-ann_query_embedding = np.fromfile("ann_query_embedding.txt").reshape(1,-1)
+ann_query_embedding = np.fromfile("ann_query_embedding.txt",dtype=np.float32).reshape(1,-1)
 # SHAPE: (1, 768) DTYPE: float32
 
 # it still needs gpu.
