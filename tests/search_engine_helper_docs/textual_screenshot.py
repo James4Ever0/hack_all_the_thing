3bc6dbe1353200f54bcc4feffaa63b2406f9481c
GitJournal <app@gitjournal.io>
2022-10-02 15:23:04 +0800

    [2022-10-02 07:23:04]
    FILE:
    tests/search_engine_helper_docs/textual_screenshot.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_screenshot.py
+++ tests/search_engine_helper_docs/textual_screenshot.py
@@ -1,6 +1,9 @@
 from textual.app import App
 from rich.console import Console
 
+
+
+
 class MyApp(App):
     async def on_load(self) -> None:
         await self.bind("y", "screenshot", "Screenshot")
