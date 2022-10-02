620640b9bba8226c3bfce533cfd51ea55d870773
GitJournal <app@gitjournal.io>
2022-10-02 21:46:38 +0800

    [2022-10-02 13:46:38]
    FILE:
    tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
+++ tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
@@ -2,6 +2,7 @@ import numpy as np
 # from txtai.embeddings import Embeddings
 
 # embeddings = Embeddings()
+embeddings = np.fromfile('ids.txt', dtype=np.float32).reshape(-1,768)
 # embeddings.load("./multilingual_index_demo") # no model out there! fuck.
 print("LOAD COMPLETE")
 ann_query_embedding = np.fromfile("ann_query_embedding.txt",dtype=np.float32).reshape(1,-1)
