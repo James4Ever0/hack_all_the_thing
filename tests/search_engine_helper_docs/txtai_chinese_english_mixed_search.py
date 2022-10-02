e56a2478ff7ff3ec74f287154c117853eafc0f95
GitJournal <app@gitjournal.io>
2022-10-02 18:33:34 +0800

    [2022-10-02 10:33:34]
    FILE:
    tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
+++ tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     embeddings = Embeddings(
         # shibing624/text2vec-base-chinese for text2vec: https://pypi.org/project/text2vec/
         # 1.11 GB, could blow my shit?
+        # since i don't speak languages other than chinese and english.
         {'path':'TingChenChang/make-multilingual-en-zh-tw-20220825062338'}
         # 539 MB
         # {'path': "sentence-transformers/distiluse-base-multilingual-cased-v2"}
