1e7e5137c34fecd5778a2380aaaffbf24d9e107d
GitJournal <app@gitjournal.io>
2022-10-02 21:44:24 +0800

    [2022-10-02 13:44:24]
    FILE:
    tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
+++ tests/search_engine_helper_docs/txtai_chinese_english_mixed_search.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     # Add index and sort desc based on score
     # save embeddings!
     # embeddings.save("./multilingual_index_demo")
-    ids = np.array(embeddings.config["ids"])
+    ids = embeddings.config["ids"]
     np.savetxt("ids.txt", ids)
     print("SHAPE", ids.shape, "DTYPE", ids.dtype) # it is a list.
     print("saving done")
