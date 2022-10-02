746fb258d58cc695daf2ccdce9bcbd7995a7081e
GitJournal <app@gitjournal.io>
2022-10-02 21:35:38 +0800

    [2022-10-02 13:35:38]
    FILE:
    tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
+++ tests/search_engine_helper_docs/txtai_load_from_index_search_saved_embeddings.py
@@ -3,7 +3,7 @@ from txtai.embeddings import Embeddings
 
 embeddings = Embeddings()
 embeddings.load("./multilingual_index_demo", model=False, query=False)
-
+print("LOAD COMPLETE")
 ann_query_embedding = np.fromfile("ann_query_embedding.txt",dtype=np.float32).reshape(1,-1)
 # SHAPE: (1, 768) DTYPE: float32
 
