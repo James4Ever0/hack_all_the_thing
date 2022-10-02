b3b3be976eab0cca62f7d5a46a945ebab265cc1a
GitJournal <app@gitjournal.io>
2022-10-02 15:22:07 +0800

    [2022-10-02 07:22:07]
    FILE:
    tests/search_engine_helper_docs/textual_screenshot.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/textual_screenshot.py
+++ tests/search_engine_helper_docs/textual_screenshot.py
@@ -8,5 +8,5 @@ class MyApp(App):
     def action_screenshot(self) -> None:
         console = Console(record=True)
         console.print(self)
-        console.save_text()
+        console.text
         console.save_svg("screenshot.svg", title="MyTitle") # not so right?
