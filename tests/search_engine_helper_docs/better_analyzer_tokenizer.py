19e0ed37233a4a3ad909ec81664a094c51bfcf00
GitJournal <app@gitjournal.io>
2022-10-03 14:25:05 +0800

    [2022-10-03 06:25:05]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -88,10 +88,9 @@ for sample in samples:
             else:
                 final_cutted_words.extend(ninja_cutted_word)
     # now 'stem' words use nltk.
-    final_stemmed_words = [stemmer.stem(word)]
-    for word in final_words:
-        stemmed_word = 
-        final_stemmed_words.append(stemmed_word)
+    final_stemmed_words = [stemmer.stem(word) for word in final_words]
+    final_stemmed_words = [stemmer.stem(word) for word in final_words]
+    
     # finally, join all things with space, for whatever reason.
     final_line = " ".join(final_words) # for our dearly transformer
     final_stemmed_line = " ".join(final_stemmed_words) # for our dearly whoosh
