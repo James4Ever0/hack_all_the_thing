9c1e675a0e54cf95005bdc2150911872f94ebad1
GitJournal <app@gitjournal.io>
2022-10-03 14:52:24 +0800

    [2022-10-03 06:52:24]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -103,8 +103,9 @@ for sample in samples:
     # use them all for all search engines? that will increase our index size significantly!
     
     # problem is, both query and data need to be processed somehow. but how?
-    
+
     # you want to use different split methods at the same time, or one at a time?
+    # you want to score them right? mostly in whoosh!
     print('final_line')
     sprint(final_line)
     print('final_cutted_line')
