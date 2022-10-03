9edf0ccd3f1e10c243eeaeda3286ada51c669e3f
GitJournal <app@gitjournal.io>
2022-10-03 14:25:32 +0800

    [2022-10-03 06:25:32]
    FILE:
    tests/search_engine_helper_docs/better_analyzer_tokenizer.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/better_analyzer_tokenizer.py
+++ tests/search_engine_helper_docs/better_analyzer_tokenizer.py
@@ -94,6 +94,7 @@ for sample in samples:
     # finally, join all things with space, for whatever reason.
     final_line = " ".join(final_words) # for our dearly transformer
     final_stemmed_line = " ".join(final_stemmed_words) # for our dearly whoosh
+    final_cutted_stemmed_line = " ".join(final_stemmed_words) # for our dearly whoosh
     from lazero.utils.logger import sprint
     print('final_line')
     sprint(final_line)
