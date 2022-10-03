adcd0c3f513f2a6af68dfe746cae1fdb33d0d60a
GitJournal <app@gitjournal.io>
2022-10-03 15:09:54 +0800

    [2022-10-03 07:09:54]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/whoosh_weighting_scoring_search_highlight_conv_sentence_piece copy.py
+++ tests/search_engine_helper_docs/whoosh_weighting_scoring_search_highlight_conv_sentence_piece copy.py
@@ -80,8 +80,8 @@ def index_search(dirname, search_fields, search_query):
         # line number? use string slice and count('\n') please.
         # or we could directly use the highlighter without whoosh?
         for hitIndex, hit in enumerate(results): # we cannot override the imported 'index'
-            score = results.scorer(hit)
-            print('HIT:',hitIndex)
+            # score = results.scorer(hit)
+            # print('HIT:',hitIndex)
             # print(hit)
             # breakpoint()
             content = hit['content']
