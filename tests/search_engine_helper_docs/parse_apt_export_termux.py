985b968f192c0d05453deeacdb3c67bd6a6ea518
modifier <termux@termux.com>
2022-11-26 21:09:43 +0800

    [2022-11-26 13:09:41]
    FILE:
    tests/search_engine_helper_docs/parse_apt_export_termux.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/parse_apt_export_termux.py
+++ tests/search_engine_helper_docs/parse_apt_export_termux.py
@@ -5,11 +5,21 @@ with open("apt_export_termux.log","r") as f:
     d=f.read()
     lines=d.split("\n")
 
+    mprog=[]
+    import parse
+    mbuf=[]
+
     for l in lines:
         if any([(b in l) for b in banned]):
             continue
         else:
             if l.strip() == "":
-                print("_"*4)
+                if mbuf == []:
+                    continue
+                else:
+                    mhead=mbuf[0]
+                    mtail=mbuf[1]
+                    #print("_"*4)
             else:
-                print(l)
+                #print(l)
+                mbuf.append(l)
