086e5b67f13bdca64d1ca9ff52d7208fc611f531
modifier <termux@termux.com>
2022-11-26 21:03:39 +0800

    [2022-11-26 13:03:37]
    FILE:
    tests/search_engine_helper_docs/parse_apt_export_termux.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/parse_apt_export_termux.py
+++ tests/search_engine_helper_docs/parse_apt_export_termux.py
@@ -1,4 +1,15 @@
 
+banned=['Sorting...','Full Text Search...']
+
 with open("apt_export_termux.log","r") as f:
     d=f.read()
+    lines=d.split("\n")
 
+    for l in lines:
+        if any([(b in l) for b in banned]):
+            continue
+        else:
+            if l.strip() == "":
+                print("_"*4)
+            else:
+                print(l)
