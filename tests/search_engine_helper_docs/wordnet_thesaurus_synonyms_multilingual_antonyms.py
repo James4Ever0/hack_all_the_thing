2fc0ac22474f198d6b23b7473ec28096172dd9c5
modifier <termux@termux.com>
2022-11-27 20:33:02 +0800

    [2022-11-27 12:32:59]
    FILE:
    tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
+++ tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
@@ -4,4 +4,5 @@ print("dog synsets?" , var)
 
 # we do not have such shit.
 var=wn.synsets('狗', lang='cmn')[0]
+# it is working, but we still don't get the language id firsthand.
 print("dog2 synsets?" , var)
