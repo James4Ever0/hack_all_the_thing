640c65ba44b86284bac14f8053e2e954001febc5
modifier <termux@termux.com>
2022-11-27 12:06:23 +0800

    [2022-11-27 04:06:20]
    FILE:
    tests/search_engine_helper_docs/help_apt_search.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/help_apt_search.sh
+++ tests/search_engine_helper_docs/help_apt_search.sh
@@ -1,3 +1,3 @@
 cat apt_export_termux.log | fzf | xargs -iabc grep -C 3 "abc" apt_export_termux.log
 
-# you can develop a processor to add similar words dynamically per line, then remove these words at the end of search
+# you can develop a processor to add similar words (thesaurus, synonyms, even translation) dynamically per line, then remove these words at the end of search
