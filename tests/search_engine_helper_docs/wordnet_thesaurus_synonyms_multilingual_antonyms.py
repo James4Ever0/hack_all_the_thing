50c9944ad0a31676746ebcd07cd0531074eca550
modifier <termux@termux.com>
2022-11-27 19:46:47 +0800

    [2022-11-27 11:46:46]
    FILE:
    tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
+++ tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
@@ -2,5 +2,6 @@ from nltk.corpus import wordnet as wn
 var=wn.synsets('dog')[0]
 print("dog synsets?" , var)
 
-var=wn.synsets('狗')[0]
-print("dog2 synsets?" , var)
+# we do not have such shit.
+#var=wn.synsets('狗')[0]
+#print("dog2 synsets?" , var)
