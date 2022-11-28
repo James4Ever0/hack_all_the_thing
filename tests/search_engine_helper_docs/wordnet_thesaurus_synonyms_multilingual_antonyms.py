1f274f6c3b1ac37092665c8e1f23500befdbac98
modifier <termux@termux.com>
2022-11-28 09:33:21 +0800

    [2022-11-28 01:33:19]
    FILE:
    tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
+++ tests/search_engine_helper_docs/wordnet_thesaurus_synonyms_multilingual_antonyms.py
@@ -10,7 +10,8 @@ var=wn.synsets('狗', lang='cmn')[0]
 print("dog2 synsets?" , var)
 
 # then? find synonyms.
-s=var.hypernyms()[0].hypernyms()
+s=var.hypernyms()[0].hyponyms()
+#not hypernyms. hyponyms.
 #s=var.hypernyms()[0].hypernyms()
 print(s)
 
