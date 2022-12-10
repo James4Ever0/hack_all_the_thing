e646a50a58eb3cf960f3fa41574c49102deff8ca
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 23:17:11 +0800

    [2022-12-10 15:17:11]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -11,26 +11,27 @@ from playwright.sync_api import sync_playwright
 # timeout = 5000  # 5 seconds? too short?
 
 
-def getBrowserInstance(p, headless=False,mProxySettings={"server": "http://127.0.0.1:38457"}):
+def getBrowserInstance(
+    p, headless=False, mProxySettings={"server": "http://127.0.0.1:38457"}
+):
     # not using proxy settings! fuck
-    browser = p.chromium.launch(headless=headless,
-        proxy=mProxySettings  # seems to be headless. fuck. i ain't see shit. you need to be patient though.
+    browser = p.chromium.launch(
+        headless=headless,
+        proxy=mProxySettings,  # seems to be headless. fuck. i ain't see shit. you need to be patient though.
     )  # that shit is faked by openai. it does not read the fucking manual carefully.
     return browser
 
 
-def browseAndSave(page, targetURL, save_path, timeout_0=5000,timeout_1 = 2000):
+def browseAndSave(page, targetURL, save_path, timeout_0=5000, timeout_1=2000):
     # page.wait_for_selector('body') # oh shit does that really work?
     # you should use some timeout strategy.]
     incomplete = True
     try:
-        page.goto(targetURL,wait_until="domcontentloaded",timeout=timeout_0)
-        page.wait_for_load_state("load",timeout=timeout_1)
+        page.goto(targetURL, wait_until="domcontentloaded", timeout=timeout_0)
+        page.wait_for_load_state("load", timeout=timeout_1)
         incomplete = False
     except:
-        print(
-            "MAYBE TIMEOUT ENCOUNTERED.\nSAVE HTML NO MATTER WHAT.\nTIMEOUT?:"
-        )
+        print("MAYBE TIMEOUT ENCOUNTERED.\nSAVE HTML NO MATTER WHAT.\nTIMEOUT?:")
 
     # Save the page HTML to a file
     html = page.content()
@@ -42,6 +43,7 @@ def browseAndSave(page, targetURL, save_path, timeout_0=5000,timeout_1 = 2000):
         save_path.seek(0)
         # named temporary file?
     return incomplete
+
 
 from read_and_scrape import getURLMap
 from readability_use_mozilla_test import read_html
@@ -60,21 +62,22 @@ with sync_playwright() as p:
     # does that work?
     urlmap = getURLMap("tools.csv")
     for targetURL, _ in progressbar.progressbar(urlmap.items()):
-    # targetURL = "https://github.com/dotnetcore/FastGithub"
+        # targetURL = "https://github.com/dotnetcore/FastGithub"
         import tempfile
+
         with tempfile.NamedTemporaryFile(suffix=".html") as f:
             save_path = f.name
             incomplete = browseAndSave(page, targetURL, save_path)
             # now you utilize the function.
             success = False
-            
+            print("FILEPATH?", save_path)
             try:
-                data = read_html(save_path) # will it succeed?
+                data = read_html(save_path)  # will it succeed?
             except:
                 data = {}
-            data.update({'success': success,'incomplete':incomplete})
+            data.update({"success": success, "incomplete": incomplete})
             key = targetURL
-            upsert_data(key,data)
+            upsert_data(key, data)
             # test the tinydb code first. please!
             # many repetitions. but could all be important. what do you want?
     # main loop you should do some thing.
