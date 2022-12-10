1e09643d15a51b406ef8b2a4d5cfb0217dc98422
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:08:23 +0800

    [2022-12-10 13:08:23]
    FILE:
    tests/blackarch_tools_readme_download/test_readability.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/test_readability.js
+++ tests/blackarch_tools_readme_download/test_readability.js
@@ -1,7 +1,7 @@
 var { Readability } = require('@mozilla/readability');
 var { JSDOM } = require('jsdom');
 var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
-  url: "https://www.baidu.com"
+  url: "https://www.baidu.com" // fine now?
 });
 let reader = new Readability(doc.window.document);
 let article = reader.parse();
