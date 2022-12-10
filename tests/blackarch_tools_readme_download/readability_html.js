69eb857cae8df61dfe7227ca8b0ddf56f7e79e64
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:52:45 +0800

    [2022-12-10 13:52:45]
    FILE:
    tests/blackarch_tools_readme_download/readability_html.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_html.js
+++ tests/blackarch_tools_readme_download/readability_html.js
@@ -1,13 +1,13 @@
-var { Readability } = require('@mozilla/readability');
-var { JSDOM } = require('jsdom');
-const fs = require("fs");
+var {Readability}=require('@mozilla/readability');
+var {JSDOM}=require('jsdom');
+const fs=require("fs");
 // mpath = "fastgithub_readme.html"
-module.exports = {
-    read_html:(mpath) => {
-    var content = fs.readFileSync(mpath, {encoding: "utf8"})
-var doc = new JSDOM(content);
-let reader = new Readability(doc.window.document);
-let article = reader.parse();
-return article;
-}
+module.exports={
+    read_html: (mpath) => {
+        var content=fs.readFileSync(mpath,{encoding: "utf8"})
+        var doc=new JSDOM(content);
+        let reader=new Readability(doc.window.document);
+        let article=reader.parse();
+        return article;
+    }
 }
