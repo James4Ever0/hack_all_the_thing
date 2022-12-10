b9a60bf1a6ca2ab88398d1edf9660a42bf4691ff
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 20:39:29 +0800

    [2022-12-10 12:39:29]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -1,19 +1,19 @@
 # let's ask openai.
 # openai you are the best.
 # import os
-from playwright.sync_api import  sync_playwright
+from playwright.sync_api import sync_playwright
 
 # Set the path to the certificate file
 # cert_path = '/opt/fastgithub/cacert/fastgithub.cer' # where is the path of fastgithub?
 targetURL = "https://github.com/dotnetcore/FastGithub"
-timeout = 5000 # 5 seconds? too short?
+timeout = 5000  # 5 seconds? too short?
 
 # Create a Playwright instance and launch browser
 with sync_playwright() as p:
-    mProxySettings = {
-              'server': '127.0.0.1:38457'
-            } # whatever.
-    browser = p.chromium.launch(proxy=mProxySettings) # that shit is faked by openai. it does not read the fucking manual carefully.
+    mProxySettings = {"server": "127.0.0.1:38457"}  # whatever.
+    browser = p.chromium.launch(
+        proxy=mProxySettings
+    )  # that shit is faked by openai. it does not read the fucking manual carefully.
     # Create a new page and set the HTTP proxy
     page = browser.new_page()
     # does that work?
@@ -23,13 +23,15 @@ with sync_playwright() as p:
     # page.wait_for_selector('body') # oh shit does that really work?
     # you should use some timeout strategy.
     try:
-        page.wait_for_load_state('networkidle',timeout=timeout)
+        page.wait_for_load_state("networkidle", timeout=timeout)
     except:
-        print("MAYBE TIMEOUT ENCOUNTERED.\nSAVE HTML NO MATTER WHAT.\nTIMEOUT?:",timeout)
+        print(
+            "MAYBE TIMEOUT ENCOUNTERED.\nSAVE HTML NO MATTER WHAT.\nTIMEOUT?:", timeout
+        )
 
     # Save the page HTML to a file
     html = page.content()
-    with open('target.html', 'w') as f:
+    with open("target.html", "w") as f:
         f.write(html)
 
     # Close the browser
