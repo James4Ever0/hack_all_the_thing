39e733595b0c50bff5388b917407b9be4b424d7b
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:53:41 +0800

    [2022-12-10 13:53:41]
    FILE:
    tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
+++ tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
@@ -7,19 +7,19 @@ read_html = require("readability_html").read_html
 # J = require("jsdom").JSDOM
 # ## name collision?
 
-# print(dir(JSDOM))
-with open("fastgithub_readme.html", "r") as f:
-    html = f.read()
-    # url = "https://github.com/dotnetcore/FastGithub"
-    # do not provide url. JSDOM won't load shit.
-    print('loading JSDOM') # shit man. do not load shit!
-    # taking forever? fuck!
-    doc = J(html) # just because you cannot load this stuff. how about create some javascript library for this task. use module.export instead.
-    print("STEP 2")
-    reader = R(doc.window.document)
-    print("STEP 3")
-    article = reader.parse()
-    print("ARTICLE?")
-    import rich
+# # print(dir(JSDOM))
+# with open("fastgithub_readme.html", "r") as f:
+#     html = f.read()
+#     # url = "https://github.com/dotnetcore/FastGithub"
+#     # do not provide url. JSDOM won't load shit.
+#     print('loading JSDOM') # shit man. do not load shit!
+#     # taking forever? fuck!
+#     doc = J(html) # just because you cannot load this stuff. how about create some javascript library for this task. use module.export instead.
+#     print("STEP 2")
+#     reader = R(doc.window.document)
+#     print("STEP 3")
+#     article = reader.parse()
+#     print("ARTICLE?")
+#     import rich
 
-    rich.print(article)
+#     rich.print(article)
