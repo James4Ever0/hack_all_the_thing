8208fa6f3042253c12e17f45b30543d52f83d6d1
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 20:17:47 +0800

    [2022-12-10 12:17:47]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -1,2 +1,36 @@
 # let's ask openai.
 # openai you are the best.
+import os
+from playwright.sync_api import Playwright, sync_playwright
+
+Set the path to the certificate file
+cert_path = '/tmp/cacert.crt'
+
+Create a Playwright instance and launch browser
+with sync_playwright() as p:
+# Set browser to use the certificate for HTTPS connections
+browser_context_options = {
+"https_certificates": [{
+"certificate_chain": [open(cert_path, "rb").read()]
+}]
+}
+browser = p.chromium.launch(browser_context_options=browser_context_options)
+
+Copy code
+# Create a new page and set the HTTP proxy
+page = browser.new_page()
+page.set_http_proxy('http://localhost:38457')
+
+# Navigate to Google and wait for the page to fully load
+page.goto('https://google.com')
+page.wait_for_selector('body')
+
+# Save the page HTML to a file
+html = page.content()
+with open('google.html', 'w') as f:
+    f.write(html)
+
+# Close the browser
+browser.close()
+
+
