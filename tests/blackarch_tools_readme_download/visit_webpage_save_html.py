2b20c09526236be009e59e90a2ac31829b3d5cf3
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:14:43 +0800

    [2022-12-10 14:14:43]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -7,7 +7,7 @@ from playwright.sync_api import sync_playwright
 
 # Set the path to the certificate file
 # cert_path = '/opt/fastgithub/cacert/fastgithub.cer' # where is the path of fastgithub?
-targetURL = "https://github.com/dotnetcore/FastGithub"
+# targetURL = "https://github.com/dotnetcore/FastGithub"
 timeout = 5000  # 5 seconds? too short?
 
 def getBrowserInstance(p,mProxySettings = {"server": "127.0.0.1:38457"} ):
@@ -16,7 +16,7 @@ def getBrowserInstance(p,mProxySettings = {"server": "127.0.0.1:38457"} ):
     )  # that shit is faked by openai. it does not read the fucking manual carefully.
     return browser
 
-def browse
+def browseAndSave(page,targetURL, save_path):
 
 # Create a Playwright instance and launch browser
 with sync_playwright() as p:
