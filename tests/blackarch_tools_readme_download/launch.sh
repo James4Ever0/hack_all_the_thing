f01df9c75b8b70cf5fc04b2ca072a9fa9c19dcdf
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:20:22 +0800

    [2022-12-10 14:20:22]
    FILE:
    tests/blackarch_tools_readme_download/launch.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/launch.sh
+++ tests/blackarch_tools_readme_download/launch.sh
@@ -3,4 +3,4 @@ bash download_main_list.sh # download the target html. in a single page.
 # now we create CSV with pandas.
 python3 extract_tools.py
 
-# we download and create excerpt from 
+# we download and create excerpts from unique URL? use some 
