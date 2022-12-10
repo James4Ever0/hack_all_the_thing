f0c6509b8a25f144f2271ad231840f04bb7f9e8f
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 20:20:24 +0800

    [2022-12-10 12:20:24]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -8,31 +8,31 @@ cert_path = '/opt/fastgithub/cacert/fastgithub.cer' # where is the path of fastg
 
 # Create a Playwright instance and launch browser
 with sync_playwright() as p:
-# Set browser to use the certificate for HTTPS connections
-browser_context_options = {
-"https_certificates": [{
-"certificate_chain": [open(cert_path, "rb").read()]
-}]
-}
-browser = p.chromium.launch(browser_context_options=browser_context_options)
+    # Set browser to use the certificate for HTTPS connections
+    browser_context_options = {
+    "https_certificates": [{
+    "certificate_chain": [open(cert_path, "rb").read()]
+    }]
+    }
+    browser = p.chromium.launch(browser_context_options=browser_context_options)
 
-# Create a new page and set the HTTP proxy
-page = browser.new_page()
-# does that work?
-page.set_http_proxy('http://localhost:38457')
-page.set_https_proxy('http://localhost:38457')
+    # Create a new page and set the HTTP proxy
+    page = browser.new_page()
+    # does that work?
+    page.set_http_proxy('http://localhost:38457')
+    page.set_https_proxy('http://localhost:38457')
 
-# Navigate to Google and wait for the page to fully load
-page.goto('https://google.com')
-# page.wait_for_selector('body') # oh shit does that really work?
-# you should use some timeout strategy.
+    # Navigate to Google and wait for the page to fully load
+    page.goto('https://google.com')
+    # page.wait_for_selector('body') # oh shit does that really work?
+    # you should use some timeout strategy.
 
-# Save the page HTML to a file
-html = page.content()
-with open('target.html', 'w') as f:
-    f.write(html)
+    # Save the page HTML to a file
+    html = page.content()
+    with open('target.html', 'w') as f:
+        f.write(html)
 
-# Close the browser
-browser.close()
+    # Close the browser
+    browser.close()
 
 
