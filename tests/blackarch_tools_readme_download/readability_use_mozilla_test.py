1250940e631c265f1c94ed0db1ab805867873292
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:53:53 +0800

    [2022-12-10 13:53:53]
    FILE:
    tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
+++ tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
@@ -8,6 +8,7 @@ read_html = require("readability_html").read_html
 # ## name collision?
 
 # # print(dir(JSDOM))
+
 # with open("fastgithub_readme.html", "r") as f:
 #     html = f.read()
 #     # url = "https://github.com/dotnetcore/FastGithub"
