080332366f8df0385e716488a8dffa341d305668
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:37:19 +0800

    [2022-12-10 13:37:19]
    FILE:
    tests/blackarch_tools_readme_download/test_readability.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/test_readability.js
+++ tests/blackarch_tools_readme_download/test_readability.js
@@ -3,6 +3,9 @@ var { JSDOM } = require('jsdom');
 var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
   url: "https://www.baidu.com" // fine now? you do not load shits from that url ?
 });
+var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
+    url: "https://www.baidu.com" // fine now? you do not load shits from that url ?
+  });
 let reader = new Readability(doc.window.document);
 let article = reader.parse();
 console.log("ARTICLE?")
