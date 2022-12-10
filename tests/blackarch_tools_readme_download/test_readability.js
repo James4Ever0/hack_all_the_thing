ddaa6c222f97653969e80313c610aff26fbe2eda
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:49:37 +0800

    [2022-12-10 13:49:37]
    FILE:
    tests/blackarch_tools_readme_download/test_readability.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/test_readability.js
+++ tests/blackarch_tools_readme_download/test_readability.js
@@ -7,6 +7,7 @@ let JSDOM = J.JSDOM // working.
 //   url: "https://www.baidu.com" // fine now? you do not load shits from that url ?
 // });
 const fs = require("fs");
+mpath = ""
 var content = fs.readFileSync(mpath, {encoding: "utf8"})
 var doc = new JSDOM(content);
 let reader = new Readability(doc.window.document);
