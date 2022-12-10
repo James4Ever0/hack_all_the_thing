208d55bac580ae8ff5bccf7c254d8bb8dc7a9926
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:01:12 +0800

    [2022-12-10 13:01:12]
    FILE:
    tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
+++ tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
@@ -1,15 +1,14 @@
 from javascript import require
 
-Readability= require('@mozilla/readability').Readability
-JSDOM = require('jsdom').JSDOM
-with open("fastgithub_readme.html",'r') as f:
+Readability = require("@mozilla/readability").Readability
+JSDOM = require("jsdom").JSDOM
+with open("fastgithub_readme.html", "r") as f:
     html = f.read()
-    url = 'https://github.com/dotnetcore/FastGithub'
-    doc =JSDOM(html, {
-  "url": url
-})
+    url = "https://github.com/dotnetcore/FastGithub"
+    doc = JSDOM(html, {"url": url})
     reader = Readability(doc.window.document)
     article = reader.parse()
     print("ARTICLE?")
     import rich
+
     rich.print(article)
