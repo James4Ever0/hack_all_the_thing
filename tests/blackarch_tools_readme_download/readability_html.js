167cc693b74c0fa1bc87a6d3e5f3380177e9b0b2
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:52:09 +0800

    [2022-12-10 13:52:08]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_html.js
+++ tests/blackarch_tools_readme_download/readability_html.js
@@ -3,7 +3,7 @@ var { JSDOM } = require('jsdom');
 const fs = require("fs");
 // mpath = "fastgithub_readme.html"
 module.exports = {
-    
+    read_html
     var content = fs.readFileSync(mpath, {encoding: "utf8"})
 var doc = new JSDOM(content);
 let reader = new Readability(doc.window.document);
