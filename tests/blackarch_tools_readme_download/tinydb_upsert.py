ba2e9719592afac4c6db6ecf7570f43bb44394c1
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:33:20 +0800

    [2022-12-10 14:33:20]
    FILE:
    tests/blackarch_tools_readme_download/tinydb_upsert.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ tests/blackarch_tools_readme_download/tinydb_upsert.py
@@ -0,0 +1,11 @@
+from tinydb import TinyDB, Query
+
+# Open a TinyDB database
+db = TinyDB()
+
+# Create a query to find the record with the key "foo"
+query = Query()
+query = query.key == "foo"
+
+# Update the value of the key "foo", creating a new record if it does not exist
+db.upsert({"key": "foo", "value": "new_value"}, query)
