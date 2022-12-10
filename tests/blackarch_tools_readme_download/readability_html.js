02b1a3e45a7d3d9874569e6282850d5b22653181
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:51:33 +0800

    [2022-12-10 13:51:33]
    FILE:
    tests/blackarch_tools_readme_download/readability_html.js
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_html.js
+++ tests/blackarch_tools_readme_download/readability_html.js
@@ -2,10 +2,6 @@ var { Readability } = require('@mozilla/readability');
 // var { JSDOM } = require('jsdom');
 var J = require('jsdom');
 let JSDOM = J.JSDOM // working.
-// let JSDOM = J
-// var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
-//   url: "https://www.baidu.com" // fine now? you do not load shits from that url ?
-// });
 const fs = require("fs");
 mpath = "fastgithub_readme.html"
 var content = fs.readFileSync(mpath, {encoding: "utf8"})
