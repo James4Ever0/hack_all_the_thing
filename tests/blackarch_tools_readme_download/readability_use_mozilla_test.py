fd1bf4150f75aa198d6141c60949d75f62cb806a
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 21:04:17 +0800

    [2022-12-10 13:04:17]
    FILE:
    tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
+++ tests/blackarch_tools_readme_download/readability_use_mozilla_test.py
@@ -6,7 +6,7 @@ with open("fastgithub_readme.html", "r") as f:
     html = f.read()
     # url = "https://github.com/dotnetcore/FastGithub"
     # do not provide url. JSDOM won't load shit.
-    print('loading JSDOM')
+    print('loading JSDOM') # shit man. do not load shit!
     doc = JSDOM(html)
     print("STEP 2")
     reader = Readability(doc.window.document)
