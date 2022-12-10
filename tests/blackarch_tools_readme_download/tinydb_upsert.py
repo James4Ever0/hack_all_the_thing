cb7e0356b41196e673f4db7bee8c7e89bc2be9ca
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:34:59 +0800

    [2022-12-10 14:34:59]
    FILE:
    tests/blackarch_tools_readme_download/tinydb_upsert.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/tinydb_upsert.py
+++ tests/blackarch_tools_readme_download/tinydb_upsert.py
@@ -4,13 +4,13 @@ dbpath = "my_database.json"
 # Open a TinyDB database
 db = TinyDB(dbpath)
 
-def upsert_data(value):
+def upsert_data(key,value):
     # Create a query to find the record with the key "foo"
     query = Query()
     query = query.key == "foo"
 
     # Update the value of the key "foo", creating a new record if it does not exist
-    db.upsert({"key": "foo", "value": value}, query)
+    db.upsert({"key": key, "value": value}, query)
     # fucking working?
 
 if __name__ == "__main__":
