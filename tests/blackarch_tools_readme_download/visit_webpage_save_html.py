6e87ead2e865e7ec6b285ccfeb1c9317f2ca6693
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 23:06:03 +0800

    [2022-12-10 15:06:03]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -12,6 +12,7 @@ from playwright.sync_api import sync_playwright
 
 
 def getBrowserInstance(p, mProxySettings={"server": "127.0.0.1:38457"}):
+    # not using proxy settings! fuck
     browser = p.chromium.launch(
         proxy=mProxySettings  # seems to be headless. fuck. i ain't see shit. you need to be patient though.
     )  # that shit is faked by openai. it does not read the fucking manual carefully.
