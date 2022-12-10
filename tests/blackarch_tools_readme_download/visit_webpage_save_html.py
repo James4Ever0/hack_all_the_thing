7595abadbffa96c05ffd2d91ad098517e2501eab
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:39:22 +0800

    [2022-12-10 14:39:22]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -63,7 +63,7 @@ with sync_playwright() as p:
             save_path = f.name
             incomplete = browseAndSave(page, targetURL, save_path)
             # now you utilize the function.
-            data = read_html(save_path)
+            data = read_html(save_path) # will it succeed?
             # test the tinydb code first. please!
             # many repetitions. but could all be important. what do you want?
     # main loop you should do some thing.
