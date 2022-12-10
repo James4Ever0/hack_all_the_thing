d8872995308261da604f2411f7962dfa7e30554c
jamesbrown <randomvoidmail@foxmail.com>
2022-12-10 22:15:44 +0800

    [2022-12-10 14:15:44]
    FILE:
    tests/blackarch_tools_readme_download/visit_webpage_save_html.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/visit_webpage_save_html.py
+++ tests/blackarch_tools_readme_download/visit_webpage_save_html.py
@@ -45,7 +45,9 @@ with sync_playwright() as p:
     # Create a new page and set the HTTP proxy
     page = browser.new_page() # just use the same damn page.
     # does that work?
-    
+# targetURL = "https://github.com/dotnetcore/FastGithub"
+
+    browseAndSave
 
     # Navigate to Google and wait for the page to fully load
     # Close the browser
