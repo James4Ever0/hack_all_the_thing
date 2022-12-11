edade60095f5465216dd25517aa54aba21643a61
jamesbrown <randomvoidmail@foxmail.com>
2022-12-11 22:28:39 +0800

    [2022-12-11 14:28:39]
    FILE:
    tests/kali_tools_info_download/parse_all.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_all.py
+++ tests/kali_tools_info_download/parse_all.py
@@ -29,7 +29,7 @@ for fname in htmls:
                     # check if there's link
                     a = elem.find('a')
                     if a is not None:
-                        print("FOUND A:",a.text)
-                        print("A LINK:",a.attr('href'))
+                        print("FOUND A:",a.text.strip())
+                        print("A LINK:",a.attrs['href'])
                     else:
                         print("elem?", elem.text.strip())
