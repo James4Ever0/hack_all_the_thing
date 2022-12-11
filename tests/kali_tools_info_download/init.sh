326f9bcfb28a1468e71775cc42d2722ab9e291ea
jamesbrown <randomvoidmail@foxmail.com>
2022-12-12 04:08:41 +0800

    [2022-12-11 20:07:00]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/init.sh
+++ tests/kali_tools_info_download/init.sh
@@ -1,3 +1,4 @@
 curl https://en.kali.tools/all/ > kali_tools_all.html # more tags, more categories, the same as blackarch?
 curl https://www.kali.org/tools/ > kali_official.html
 curl https://en.kali.tools/ > pentest_tools_with_name.html
+# there's also kali-meta page, kali package index, containing homepage link.
