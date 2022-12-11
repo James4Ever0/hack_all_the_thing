f6e5ce57b00b14d984b098e47cc2a519412c7b8f
jamesbrown <randomvoidmail@foxmail.com>
2022-12-12 04:35:07 +0800

    [2022-12-11 20:35:07]
    FILE:
    tests/kali_tools_info_download/init.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/init.sh
+++ tests/kali_tools_info_download/init.sh
@@ -21,6 +21,6 @@ curl https://en.kali.tools/ > pentest_tools_with_name.html
 # https://pypi.org/project/pypi-command-line/
 # install it.
 # run: pypi search <query>
-# it also provides "read-the-docs" to search for documentation
+# it also provides "read-the-docs" to search in documentation of a package
 
 # you can download man pages before installing package
