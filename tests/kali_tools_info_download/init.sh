bab37604c4093a9dfc927ccce3c41e465c1c0772
jamesbrown <randomvoidmail@foxmail.com>
2022-12-12 04:09:40 +0800

    [2022-12-11 20:09:40]
    FILE:
    tests/kali_tools_info_download/init.sh
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/init.sh
+++ tests/kali_tools_info_download/init.sh
@@ -2,3 +2,4 @@ curl https://en.kali.tools/all/ > kali_tools_all.html # more tags, more categori
 curl https://www.kali.org/tools/ > kali_official.html
 curl https://en.kali.tools/ > pentest_tools_with_name.html
 # there's also kali-meta page, kali package index, containing homepage link.
+# kali meta page 
