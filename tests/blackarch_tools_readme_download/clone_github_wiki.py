d06934cecfdd1ba1b244d8070c5c17c4434a21cf
jamesbrown <randomvoidmail@foxmail.com>
2022-12-12 19:17:38 +0800

    [2022-12-12 11:17:38]
    FILE:
    tests/blackarch_tools_readme_download/clone_github_wiki.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/clone_github_wiki.py
+++ tests/blackarch_tools_readme_download/clone_github_wiki.py
@@ -1,6 +1,7 @@
 import parse
 
 def check_if_is_repo(url):
+    url = url.strip("/")
     format = "https://github.com/{user}/{repo}"
     if parse.parse(format, url):
         wiki_url = url+".wiki.git" # simply wrong
