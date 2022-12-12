ba2670458419d40c65aaaed45a4a803fdf4a8a74
jamesbrown <randomvoidmail@foxmail.com>
2022-12-12 19:22:03 +0800

    [2022-12-12 11:22:03]
    FILE:
    tests/blackarch_tools_readme_download/clone_github_wiki.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/clone_github_wiki.py
+++ tests/blackarch_tools_readme_download/clone_github_wiki.py
@@ -1,13 +1,18 @@
 import parse
 import os
 os.mkdir("github_wiki")
-
+os.chdir("github_wiki")
 # use --depth=1
 
 def gen_git_clone_command(wiki_url):
+    return "git clone --depth=1 {}".format(wiki_url)
 
 def check_if_is_repo(url):
     url = url.strip("/")
     format = "https://github.com/{user}/{repo}"
     if parse.parse(format, url):
         wiki_url = url+".wiki.git" # simply wrong
+
+import pandas as pd
+
+from read_and_scrape import getURLMap
