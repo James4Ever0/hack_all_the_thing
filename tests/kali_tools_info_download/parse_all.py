5748c48b9e795d1d91b5ad7873a66a585c9412b4
jamesbrown <randomvoidmail@foxmail.com>
2022-12-15 05:09:33 +0800

    [2022-12-14 21:09:33]
    FILE:
    tests/kali_tools_info_download/parse_all.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_all.py
+++ tests/kali_tools_info_download/parse_all.py
@@ -43,6 +43,7 @@ for fname in htmls:
             dict_list_write_to_csv("kali_official.csv",data)
         elif fname == "kali_tools_all.html" and flag == 1:
             mtables = soup.find_all("table",class_="table")
+            mtables = soup.find_all("table",class_="table")
             print("TOTAL TABLES?",len(mtables))
         elif fname == "pentest_tools_with_name.html" and flag == 2:
             data = {"heading": [], "name": [], "link": []}
