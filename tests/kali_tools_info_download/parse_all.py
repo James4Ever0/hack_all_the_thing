f046340868bd7995dc368624cd3d52207ec6f377
jamesbrown <randomvoidmail@foxmail.com>
2022-12-15 05:12:51 +0800

    [2022-12-14 21:12:51]
    FILE:
    tests/kali_tools_info_download/parse_all.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_all.py
+++ tests/kali_tools_info_download/parse_all.py
@@ -50,8 +50,8 @@ for fname in htmls:
             # print("TABLE?",mtable)
             df = pd.read_html(mtable)[0]
             #df = pd.read_html(tfname)[0]
-            print("head?")
-            print(df.head())
+            # print("head?")
+            # print(df.head())
         elif fname == "pentest_tools_with_name.html" and flag == 2:
             data = {"heading": [], "name": [], "link": []}
             cname = "main-content"
