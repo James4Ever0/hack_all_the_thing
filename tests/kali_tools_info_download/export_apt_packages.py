0eed9647916a6f82d2023f33d2df27480e490130
GitJournal <app@gitjournal.io>
2022-12-16 01:05:53 +0800

    [2022-12-15 17:05:53]
    FILE:
    tests/kali_tools_info_download/export_apt_packages.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -4,3 +4,7 @@ output = subprocess.check_output(["apt","list"])
 #breakpoint()
 # skip first "Listing..." line.
 from io import StringIO 
+
+mio = StringIO(output.decode("utf-8"))
+for line in mio.readline():
+    print("LINE?",line)
