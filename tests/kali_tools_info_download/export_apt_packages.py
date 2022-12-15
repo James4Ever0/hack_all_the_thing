c1c3210b9d1b439468b2844269e13e4ffd86ddcb
GitJournal <app@gitjournal.io>
2022-12-16 02:06:30 +0800

    [2022-12-15 18:06:28]
    FILE:
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -38,6 +38,7 @@ for pname in pg.progressbar(packageNames):
     if info is not None:
         data['Name'].append(pname)
         mdict = info.named
+        mdict = {key:val.strip() for key,val in mdict.items()}
         for col in targetCols:
             data[col].append(mdict.get(col,None))
             # the Result class is composed of "fixed" and "named", cannot be directly converted to dictionary
