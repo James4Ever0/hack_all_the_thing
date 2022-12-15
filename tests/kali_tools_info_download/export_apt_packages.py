a5ad3c4e2033aab5699bf78d4f095d3703e65700
GitJournal <app@gitjournal.io>
2022-12-16 03:33:06 +0800

    [2022-12-15 19:33:06]
    FILE:
    tests/kali_tools_info_download/export_apt_packages.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -34,7 +34,7 @@ import pandas as pd
 import progressbar as pg
 for pname in pg.progressbar(packageNames):
     from parse_apt_info import parse_apt_info
-    infos = parse_apt_info(pname)
+    infos = parse_apt_info(pnames)
     for info in infos:
         if info is not None:
             data['Name'].append(pname)
