39c463ecee93c54e8ac8edb286e95eaf3451cddf
GitJournal <app@gitjournal.io>
2022-12-16 03:47:25 +0800

    [2022-12-15 19:47:25]
    FILE:
    tests/kali_tools_info_download/export_apt_packages.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -42,10 +42,10 @@ for pnames in pg.progressbar(iter(lambda: iter(islice(packageNames,n)),())
     from parse_apt_info import parse_apt_info
     infos = parse_apt_info(pnames,debug=True)
     for info in infos:
+            mdict = info.named
         if info is not None:
             pname = 
             data['Name'].append(pname)
-            mdict = info.named
             mdict = {key:val.strip() for key,val in mdict.items()}
             for col in targetCols:
                 data[col].append(mdict.get(col,None))
