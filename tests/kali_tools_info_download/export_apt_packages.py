3fb2875db702a0dc9e5623aa343bfb8841cef977
GitJournal <app@gitjournal.io>
2022-12-16 03:30:25 +0800

    [2022-12-15 19:30:25]
    FILE:
    tests/kali_tools_info_download/export_apt_packages.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -36,16 +36,16 @@ for pname in pg.progressbar(packageNames):
     from parse_apt_info import parse_apt_info
     infos = parse_apt_info(pname)
     for info in infos:
-    if info is not None:
-        data['Name'].append(pname)
-        mdict = info.named
-        mdict = {key:val.strip() for key,val in mdict.items()}
-        for col in targetCols:
-            data[col].append(mdict.get(col,None))
-            # the Result class is composed of "fixed" and "named", cannot be directly converted to dictionary
-        print({x:info[x] for x in targetCols if x in mdict.keys()})
-    else:
-        #parse_apt_info(pname,debug=True)
-        breakpoint()
+        if info is not None:
+            data['Name'].append(pname)
+            mdict = info.named
+            mdict = {key:val.strip() for key,val in mdict.items()}
+            for col in targetCols:
+                data[col].append(mdict.get(col,None))
+                # the Result class is composed of "fixed" and "named", cannot be directly converted to dictionary
+            print({x:info[x] for x in targetCols if x in mdict.keys()})
+        else:
+            #parse_apt_info(pname,debug=True)
+            breakpoint()
 df = pd.DataFrame(data)
 df.to_csv("kali_apt_packages_info.csv",index=False)
