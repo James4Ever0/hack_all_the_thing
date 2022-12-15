6df3c30c1026368a0aabd645fb3902fbaba68bc9
GitJournal <app@gitjournal.io>
2022-12-16 03:44:49 +0800

    [2022-12-15 19:44:49]
    FILE:
    tests/kali_tools_info_download/parse_apt_info.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_apt_info.py
+++ tests/kali_tools_info_download/parse_apt_info.py
@@ -49,5 +49,5 @@ def parse_apt_info(packageNames,debug=False,limit=10):
 
 if __name__ == "__main__":
     pname = "python3"
-    i = parse_apt_info([pname,'apt']).__next__()
-    print(i)
+    for i in parse_apt_info([pname,'apt']).__next__()
+        print(i)
