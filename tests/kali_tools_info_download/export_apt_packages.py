e8c5f4f6504b680795e88604729ef93ac82fad45
GitJournal <app@gitjournal.io>
2022-12-16 01:08:56 +0800

    [2022-12-15 17:08:56]
    FILE:
    tests/kali_tools_info_download/export_apt_packages.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/export_apt_packages.py
+++ tests/kali_tools_info_download/export_apt_packages.py
@@ -6,7 +6,6 @@ output = subprocess.check_output(["apt","list"])
 from io import StringIO 
 formatstr = "{packageName}/{OSName} {versionNum} {platform}"
 mio = StringIO(output.decode("utf-8"))
-init=False
 for line in mio.readlines():
 #    print("LINE?",line)
     mline = line.strip()
