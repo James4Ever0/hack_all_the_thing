c9e9f26dd4264220ab513f570b6d1a7ac90ac29a
GitJournal <app@gitjournal.io>
2022-12-16 01:21:22 +0800

    [2022-12-15 17:21:22]
    FILE:
    tests/kali_tools_info_download/parse_apt_info.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_apt_info.py
+++ tests/kali_tools_info_download/parse_apt_info.py
@@ -4,6 +4,8 @@ def parse_apt_info(packageName):
     cmd = ["apt","show",packageName]
     import subprocess
     output = subprocess.check_output(cmd)
+    so = output.decode('utf-8')
+    print(so)
 
 if __name__ == "__main__":
     pname = "python3"
