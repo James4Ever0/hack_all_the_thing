9eb8bc2cfd11e002b9bdca082028c5e37f0a791e
GitJournal <app@gitjournal.io>
2022-12-16 01:23:10 +0800

    [2022-12-15 17:23:10]
    FILE:
    tests/kali_tools_info_download/parse_apt_info.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/kali_tools_info_download/parse_apt_info.py
+++ tests/kali_tools_info_download/parse_apt_info.py
@@ -1,5 +1,30 @@
 import parse
-
+fstring = """Package: python3
+Version: 3.10.6-1
+Priority: optional
+Section: python
+Source: python3-defaults
+Maintainer: Matthias Klose <doko@debian.org>
+Installed-Size: 92.2 kB
+Provides: python3-profiler
+Pre-Depends: python3-minimal (= 3.10.6-1)
+Depends: python3.10 (>= 3.10.6-1~), libpython3-stdlib (= 3.10.6-1)
+Suggests: python3-doc (>= 3.10.6-1), python3-tk (>= 3.10.6-1~), python3-venv (>= 3.10.6-1)
+Replaces: python3-minimal (<< 3.1.2-2)
+Homepage: https://www.python.org/
+Tag: devel::interpreter, devel::lang:python, devel::library,
+ implemented-in::c, implemented-in::python, role::devel-lib,
+ role::program, role::shared-lib
+Download-Size: 38.2 kB
+APT-Sources: http://mirrors.aliyun.com/kali kali-rolling/main amd64 Packages
+Description: interactive high-level object-oriented language (default python3 version)
+ Python, the high-level, interactive object oriented language,
+ includes an extensive class library with lots of goodies for
+ network programming, system administration, sounds and graphics.
+ .
+ This package is a dependency package, which depends on Debian's default
+ Python 3 version (currently v3.10).
+"""
 def parse_apt_info(packageName):
     cmd = ["apt","show",packageName]
     import subprocess
