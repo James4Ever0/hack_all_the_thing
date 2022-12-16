33f21e04f78edaec372cd5ca2e934ec21e48eef5
GitJournal <app@gitjournal.io>
2022-12-17 06:50:37 +0800

    [2022-12-16 22:50:37]
    FILE:
    tests/blackarch_tools_readme_download/pacman_dump.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/pacman_dump.py
+++ tests/blackarch_tools_readme_download/pacman_dump.py
@@ -9,9 +9,9 @@ sio = io.StringIO(output.decode('utf-8')+"\n\n")
 sec = []
 for line in sio.readlines():
     mline =line.strip()
-    if line !="":
+    if mline !="":
         sec.append(line)
-    if line == "" and sec !=[]:
+    if mline == "" and sec !=[]:
         # process this sec.
         for s in sec:
             if s[0]!=""
