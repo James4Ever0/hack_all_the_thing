33d40c82f155ad465a8177269a7a40fc01094426
GitJournal <app@gitjournal.io>
2022-12-17 06:53:44 +0800

    [2022-12-16 22:53:44]
    FILE:
    tests/blackarch_tools_readme_download/pacman_dump.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/pacman_dump.py
+++ tests/blackarch_tools_readme_download/pacman_dump.py
@@ -14,5 +14,5 @@ for line in sio.readlines():
     if mline == "" and sec !=[]:
         # process this sec.
         for s in sec:
-            if (s[0]isalnum): # better analyze it. what is it?
+            if s[0].isalnum(): # better analyze it. what is it?
         sec = []
