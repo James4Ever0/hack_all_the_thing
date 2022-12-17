d87b801623ac43f98a24be3eb7c1dd6ec5e22ee4
GitJournal <app@gitjournal.io>
2022-12-17 08:38:04 +0800

    [2022-12-17 00:38:04]
    FILE:
    tests/blackarch_tools_readme_download/pacman_dump.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/blackarch_tools_readme_download/pacman_dump.py
+++ tests/blackarch_tools_readme_download/pacman_dump.py
@@ -54,7 +54,6 @@ for line in pg.progressbar(sio.readlines()):
                     for k in targets:
                         v = mdict.get(k,None)
                         data[k].append(v)
-                except:                    e.print_exc()
-
+                except:                   
                     breakpoint()
         sec = []
